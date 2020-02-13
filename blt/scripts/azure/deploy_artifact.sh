b44a1b9879ebdbfdcd9b98a95e012d6cb292e0e9
Mike Madison <mike.madison@acquia.com>
2020-02-13 12:54:17 -0800

    updating local version of scripts.

--- blt/scripts/azure/deploy_artifact.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -ev
-
-blt artifact:deploy --commit-msg "Automated commit by Azure Pipelines for Build ${Build.BuildId}" --branch "${Build.SourceBranch}-build" --no-interaction --verbose
-
-set +v
