0c843e741937788c3cbdce91fee08312fc19f8f0
Szilveszter Marta <Szilveszter_Marta@epam.com>
2020-07-30 18:55:27 +0200

    AB#177286 separator image fix, parallax background work in progress

--- docroot/themes/custom/emulsifymars/components/02-molecules/content-feature/content-feature.stories.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/content-feature/content-feature.stories.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useEffect } from '@storybook/client-api';
 import contentFeature from './content-feature.twig';
 import contentFeatureData from './content-feature.yml';
 
+import './content-feature';
+
 export default { title: 'Molecules/Content Feature' };
 
-export const contentFeatureModule = () => (
-  <div dangerouslySetInnerHTML={{ __html: contentFeature(contentFeatureData) }} />
-);
+export const contentFeatureModule = () => {
+  useEffect(() => Drupal.attachBehaviors(), []);
+  return <div dangerouslySetInnerHTML={{ __html: contentFeature(contentFeatureData) }} />;
+    
+};
