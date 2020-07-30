de5795cb391c32575623d55d0e6167a91bf30990
Anton Tsyhanok <anton.tsyhanok@effem.com>
2020-07-30 12:22:23 +0300

    Task_177292_footer: WIP: Update footer appearance

--- docroot/themes/custom/emulsifymars/components/02-molecules/menus/menus.stories.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/menus/menus.stories.js
@@ -3,11 +3,15 @@ import { useEffect } from '@storybook/client-api';
 
 import breadcrumb from './breadcrumbs/breadcrumbs.twig';
 import inlineMenu from './inline/inline-menu.twig';
+import footerMenu from './footer/footer-menu.twig';
+import secondaryFooterMenu from './secondary-footer/secondary-footer-menu.twig';
 import mainMenu from './main-menu/main-menu.twig';
 import socialMenu from './social/social-menu.twig';
 
 import breadcrumbsData from './breadcrumbs/breadcrumbs.yml';
 import inlineMenuData from './inline/inline-menu.yml';
+import footerMenuData from './footer/footer-menu.yml';
+import secondaryFooterMenuData from './secondary-footer/secondary-footer-menu.yml';
 import mainMenuData from './main-menu/main-menu.yml';
 import socialMenuData from './social/social-menu.yml';
 
@@ -23,6 +27,12 @@ export const breadcrumbs = () => (
 );
 export const inline = () => (
   <div dangerouslySetInnerHTML={{ __html: inlineMenu(inlineMenuData) }} />
+);
+export const footer = () => (
+  <div dangerouslySetInnerHTML={{ __html: footerMenu(footerMenuData) }} />
+);
+export const secondaryFooter = () => (
+  <div dangerouslySetInnerHTML={{ __html: secondaryFooterMenu(secondaryFooterMenuData) }} />
 );
 export const main = () => {
   useEffect(() => Drupal.attachBehaviors(), []);
--- docroot/themes/custom/emulsifymars/components/03-organisms/site/site.stories.js
+++ docroot/themes/custom/emulsifymars/components/03-organisms/site/site.stories.js
@@ -5,9 +5,14 @@ import footerTwig from './site-footer/site-footer.twig';
 import siteHeader from './site-header/site-header.twig';
 
 import footerSocial from '../../02-molecules/menus/social/social-menu.yml';
-import footerMenu from '../../02-molecules/menus/inline/inline-menu.yml';
+import footerMenu from '../../02-molecules/menus/footer/footer-menu.yml';
 import secondaryMenuData from '../../02-molecules/menus/inline/inline-menu-header.yml';
 import mainMenuData from '../../02-molecules/menus/main-menu/main-menu.yml';
+
+import secondaryFooterMenu from '../../02-molecules/menus/secondary-footer/secondary-footer-menu.twig';
+import secondaryFooterMenuData from '../../02-molecules/menus/secondary-footer/secondary-footer-menu.yml';
+
+import siteFooterData from './site-footer/site-footer.yml';
 
 import '../../02-molecules/menus/main-menu/main-menu';
 
@@ -19,7 +24,7 @@ export default { title: 'Organisms/Site' };
 export const footer = () => (
   <div
     dangerouslySetInnerHTML={{
-      __html: footerTwig({ ...footerSocial, ...footerMenu }),
+      __html: footerTwig({ ...footerSocial, ...footerMenu, ...siteFooterData, ...secondaryFooterMenu, secondaryFooterMenuData }),
     }}
   />
 );
