4cf319d2c100c80a48df7f851eee1ddabb7414c9
Anton Tsyhanok <anton.tsyhanok@effem.com>
2020-08-03 20:28:42 +0300

    AB#177299: update footer template and styles.

--- docroot/themes/custom/emulsifymars/components/02-molecules/region-selector/region-selector.stories.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/region-selector/region-selector.stories.js
@@ -7,7 +7,7 @@ import regionSelectorData from './region-selector.yml';
 /**
  * Storybook Definition.
  */
-export default { title: 'Molecules/regionSelector' };
+export default { title: 'Molecules/RegionSelector' };
 
 export const regionSelectorEx = () => (
   <div dangerouslySetInnerHTML={{ __html: regionSelector(regionSelectorData) }} />
