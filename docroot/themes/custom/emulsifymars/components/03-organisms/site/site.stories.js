db7d5373379d465b698ebbad699de6c4aed03244
Dzianis Prudnikau <dzianis.prudnikau@effem.com>
2020-08-05 11:39:20 +0300

    AB#177262: FE header component.

--- docroot/themes/custom/emulsifymars/components/03-organisms/site/site.stories.js
+++ docroot/themes/custom/emulsifymars/components/03-organisms/site/site.stories.js
@@ -12,6 +12,7 @@ import mainMenuData from '../../02-molecules/menus/main-menu/main-menu.yml';
 import legalLinksData from '../../02-molecules/menus/legal-links/legal-links-menu.yml';
 
 import siteFooterData from './site-footer/site-footer.yml';
+import siteHeaderData from './site-header/site-header.yml';
 
 import '../../02-molecules/menus/main-menu/main-menu';
 
@@ -40,6 +41,7 @@ export const header = () => {
         __html: siteHeader({
           ...mainMenuData,
           ...secondaryMenuData,
+          ...siteHeaderData,
         }),
       }}
     />
