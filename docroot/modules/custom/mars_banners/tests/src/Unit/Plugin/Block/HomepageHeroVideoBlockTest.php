fae28c17f947a8e4f8c742aafaf66d7c61de7f33
Barys Nikulin <Barys_Nikulin@epam.com>
2020-08-06 17:02:48 +0300

    AB#174284: add homepage hero (video) component.

---
+++ docroot/modules/custom/mars_banners/tests/src/Unit/Plugin/Block/HomepageHeroVideoBlockTest.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace Drupal\Tests\mars_banners\Unit\Plugin\Block;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\StringTranslation\TranslationInterface;
+use Drupal\mars_banners\Plugin\Block\HomepageHeroVideoBlock;
+use Drupal\Tests\UnitTestCase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * @coversDefaultClass \Drupal\mars_banners\Plugin\Block\HomepageHeroVideoBlock
+ * @group mars
+ * @group mars_banners
+ */
+class HomepageHeroVideoBlockTest extends UnitTestCase {
+
+  private const TEST_CONFIGURATION = [
+    'id' => 'homepage_hero_video_block',
+    'label' => 'Homepage hero video block',
+    'provider' => 'mars_banners',
+    'label_display' => '0',
+    'eyebrow' => 'test eyebrow',
+    'cta' => [
+      'url' => 'https://test.test',
+      'title' => 'Explore',
+    ],
+    'background_video' => 'background video',
+  ];
+
+  private const TEST_DEFENITION = [
+    'provider' => 'mars_banners',
+    'admin_label' => 'admin_label',
+  ];
+
+  /**
+   * System under test.
+   *
+   * @var \Drupal\mars_banners\Plugin\Block\HomepageHeroVideoBlock
+   */
+  private $homepageBlock;
+
+  /**
+   * Mock.
+   *
+   * @var \Symfony\Component\DependencyInjection\ContainerInterface|\PHPUnit\Framework\MockObject\MockObject
+   */
+  private $containerMock;
+
+  /**
+   * Mock.
+   *
+   * @var \PHPUnit\Framework\MockObject\MockObject|\Drupal\Core\Form\FormStateInterface
+   */
+  private $formStateMock;
+
+  /**
+   * Mock.
+   *
+   * @var \PHPUnit\Framework\MockObject\MockObject|\Drupal\Core\StringTranslation\TranslationInterface
+   */
+  private $translationMock;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+    $this->createMocks();
+    \Drupal::setContainer($this->containerMock);
+    $this->homepageBlock = new HomepageHeroVideoBlock(
+      self::TEST_CONFIGURATION,
+      'homepage_hero_video_block',
+      self::TEST_DEFENITION
+    );
+  }
+
+  /**
+   * Test.
+   */
+  public function testShouldBuild() {
+    $block_build = $this->homepageBlock->build();
+    $this->assertSame(
+      'Homepage hero video block',
+      $block_build['#title']
+    );
+    $this->assertIsArray($block_build);
+  }
+
+  /**
+   * Test.
+   */
+  public function testShouldBuildConfigurationForm() {
+    $form_array = [];
+
+    $this->containerMock
+      ->expects($this->any())
+      ->method('get')
+      ->willReturnMap(
+        [
+          [
+            'string_translation',
+            ContainerInterface::EXCEPTION_ON_INVALID_REFERENCE,
+            $this->translationMock,
+          ],
+        ]
+      );
+
+    $block_form = $this->homepageBlock->buildConfigurationForm(
+      $form_array,
+      $this->formStateMock
+    );
+    $this->assertSame(
+      'textfield',
+      $block_form['eyebrow']['#type']
+    );
+    $this->assertIsArray($block_form);
+  }
+
+  /**
+   * Test.
+   */
+  public function testShouldBlockSubmit() {
+    $form_data = [];
+
+    $this->formStateMock
+      ->expects($this->once())
+      ->method('getValues')
+      ->willReturn(self::TEST_CONFIGURATION);
+
+    $this->homepageBlock->blockSubmit(
+      $form_data,
+      $this->formStateMock
+    );
+  }
+
+  /**
+   * Create all mocks for tests in this file.
+   */
+  private function createMocks(): void {
+    $this->containerMock = $this->createMock(ContainerInterface::class);
+    $this->formStateMock = $this->createMock(FormStateInterface::class);
+    $this->translationMock = $this->createMock(TranslationInterface::class);
+  }
+
+}
