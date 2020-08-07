e0ba60082c33df8a1d4e104a566048277d00f166
Szilveszter Marta <Szilveszter_Marta@epam.com>
2020-08-07 16:21:32 +0200

    AB#179268: badge atom

---
+++ docroot/themes/custom/emulsifymars/components/01-atoms/badge/badge.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import badge from './badge.twig';
+import badgeData from './badge.yml';
+
+export default { title: 'Atoms/Badge' };
+
+export const badgeExample = () => (
+  <div dangerouslySetInnerHTML={{ __html: badge(badgeData) }} />
+);
