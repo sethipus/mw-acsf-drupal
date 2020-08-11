9de6be61b176a6fcd7767402e8830d4a87a949e4
Oditszapc <nagykifla@gmail.com>
2020-08-11 13:37:48 +0200

    AB#179256 fixing layout

--- docroot/themes/custom/emulsifymars/components/02-molecules/product-feature/product-feature.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/product-feature/product-feature.js
@@ -1,14 +1,18 @@
 Drupal.behaviors.contentFeature = {
   attach(context) {
+    const bubble_1_top = context.getElementById('product-feature__bubble_1').getBoundingClientRect().top;
+    const bubble_2_top = context.getElementById('product-feature__bubble_2').getBoundingClientRect().top;
+    const bubble_3_top = context.getElementById('product-feature__bubble_3').getBoundingClientRect().top;
+
     window.addEventListener('scroll', () => {
       const bubble_1 = context.getElementById('product-feature__bubble_1');
       const bubble_2 = context.getElementById('product-feature__bubble_2');
       const bubble_3 = context.getElementById('product-feature__bubble_3');
       const offset = window.pageYOffset;
 
-      bubble_1.style.backgroundPosition = `0 ${70 - (offset * .3)}px`;
-      bubble_2.style.backgroundPosition = `0 ${200 - (offset * .3)}px`;
-      bubble_3.style.backgroundPosition = `0 ${450 - (offset * .3)}px`;
+      bubble_1.style.top = `${bubble_1_top - (offset * .75)}px`;
+      bubble_2.style.top = `${bubble_2_top - (offset * .75)}px`;
+      bubble_3.style.top = `${bubble_3_top - (offset * .75)}px`;
     })
 
     const isInViewport = element => {
