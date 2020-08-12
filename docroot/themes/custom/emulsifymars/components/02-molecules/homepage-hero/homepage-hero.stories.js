28c73664514ee1de1d47df9d02ec8af5921c3bd2
Oleksii Yashchuk <oleksii.yashchuk@effem.com>
2020-08-12 08:36:10 +0000

    AB#179278: Refactored, and also ticket changed to correct.

--- docroot/themes/custom/emulsifymars/components/02-molecules/homepage-hero/homepage-hero.stories.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/homepage-hero/homepage-hero.stories.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import homepageHero from './homepage-hero.twig';
-import homepageHeroData from './homepage-hero.yml';
+import homepageHero from './3up/homepage-hero.twig';
+import homepageHeroData from './3up/homepage-hero.yml';
 
 /**
  * Storybook Definition.
  */
-export default { title: 'Molecules/Homepage hero' };
+export default { title: 'Molecules/3UP/Homepage hero 3UP' };
 
 export const homepageHeroBlock = () => {
   return <div dangerouslySetInnerHTML={{ __html: homepageHero(homepageHeroData) }} />;
