fb50d36a4c450b8f222c3577b330ef3dee2fc4cf
Olga Rabodzei <olga.rabodzei@effem.com>
2020-08-13 14:50:10 +0000

    AB#179274: DI for recipe detail hero.

--- docroot/modules/custom/mars_recipes/src/Plugin/Block/RecipeDetailHero.php
+++ docroot/modules/custom/mars_recipes/src/Plugin/Block/RecipeDetailHero.php
@@ -3,11 +3,16 @@
 namespace Drupal\mars_recipes\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Plugin\ContextAwarePluginInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Access\AccessResult;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 
 /**
+ * Class RecipeDetailHero.
+ *
  * @Block(
  *   id = "recipe_detail_hero",
  *   admin_label = @Translation("Recipe detail hero"),
@@ -16,16 +21,44 @@ use Drupal\Core\Access\AccessResult;
  *     "node" = @ContextDefinition("entity:node", label = @Translation("Recipe"))
  *   }
  * )
+ *
+ * @package Drupal\mars_recipes\Plugin\Block
  */
-class RecipeDetailHero extends BlockBase implements ContextAwarePluginInterface {
+class RecipeDetailHero extends BlockBase implements ContextAwarePluginInterface, ContainerFactoryPluginInterface {
+
+  /**
+   * A view builder instance.
+   *
+   * @var \Drupal\Core\Entity\EntityViewBuilderInterface
+   */
+  protected $viewBuilder;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->viewBuilder = $entity_type_manager->getViewBuilder('node');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
     $node = $this->getContextValue('node');
-    $view_builder = \Drupal::entityTypeManager()->getViewBuilder('node');
-    return $view_builder->view($node, 'teaser');
+    return $this->viewBuilder->view($node, 'teaser');
   }
 
   /**
@@ -34,6 +67,5 @@ class RecipeDetailHero extends BlockBase implements ContextAwarePluginInterface 
   protected function blockAccess(AccountInterface $account) {
     return AccessResult::allowedIf($this->getContextValue('node')->bundle() == 'recipe');
   }
-
 
 }
