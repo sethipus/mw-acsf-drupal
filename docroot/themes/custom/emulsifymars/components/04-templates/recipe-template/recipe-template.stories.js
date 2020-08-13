2be40c5d2cd416ff5980143fe965c84b04c70997
Anton Tsyhanok <anton.tsyhanok@effem.com>
2020-08-14 00:03:48 +0300

    AB#179276: Create recipe hero module component.

--- docroot/themes/custom/emulsifymars/components/04-templates/recipe-template/recipe-template.stories.js
+++ docroot/themes/custom/emulsifymars/components/04-templates/recipe-template/recipe-template.stories.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import recipeTemplateTwig from './recipe-template.twig';
 import recipeTemplateData from './recipe-template.yml';
+import recipeHeroModuleData from '../../02-molecules/recipe-hero-module/recipe-hero-module.yml';
 import recipeSocial from '../../02-molecules/menus/social/social-menu.yml';
 
 /**
@@ -12,6 +13,7 @@ export default { title: 'Templates/Recipe Template' };
 export const recipeTemplate = () => (
   <div dangerouslySetInnerHTML={{ __html: recipeTemplateTwig({
       ...recipeTemplateData,
+      ...recipeHeroModuleData,
       ...recipeSocial
     }) }} />
 );
