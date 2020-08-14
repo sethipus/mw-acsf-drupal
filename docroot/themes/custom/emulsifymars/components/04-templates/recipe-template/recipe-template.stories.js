6691b9c75590b15e4b15ad56ef7188e8b8228802
Anton Tsyhanok <anton.tsyhanok@effem.com>
2020-08-14 23:04:20 +0300

    AB#179276: Update Recipe Template.

--- docroot/themes/custom/emulsifymars/components/04-templates/recipe-template/recipe-template.stories.js
+++ docroot/themes/custom/emulsifymars/components/04-templates/recipe-template/recipe-template.stories.js
@@ -4,6 +4,7 @@ import recipeTemplateTwig from './recipe-template.twig';
 import recipeTemplateData from './recipe-template.yml';
 import recipeHeroModuleData from '../../02-molecules/recipe-hero-module/recipe-hero-module.yml';
 import recipeSocial from '../../02-molecules/menus/social/social-menu.yml';
+import recipeBodyData from '../../03-organisms/recipe-body/recipe-body.yml';
 
 /**
  * Storybook Definition.
@@ -14,7 +15,8 @@ export const recipeTemplate = () => (
   <div dangerouslySetInnerHTML={{ __html: recipeTemplateTwig({
       ...recipeTemplateData,
       ...recipeHeroModuleData,
-      ...recipeSocial
+      ...recipeSocial,
+      ...recipeBodyData
     }) }} />
 );
 
