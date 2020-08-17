78cae6c4677adf6f2d8b0beede81b15f2d858a94
yashcole <oleksii.yashchuk@effem.com>
2020-08-17 13:48:40 +0000

    AB#179332: Article header, prepared for review.

--- docroot/themes/custom/emulsifymars/components/02-molecules/article-header/article-header.stories.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/article-header/article-header.stories.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import articleHeader from './article-header.twig';
-import articleHeaderData from './article-header.yml';
+import articleHeaderImage from './article-header-image.twig';
+import articleHeaderImageData from './article-header-image.yml';
+import articleHeaderNoImage from './article-header-noimage.twig';
+import articleHeaderNoImageData from './article-header-noimage.yml';
 
 export default { title: 'Molecules/Article Header' };
 
-export const articleHeaderLayout = () => {
-  return <div dangerouslySetInnerHTML={{ __html: articleHeader(articleHeaderData) }} />;
+export const articleHeaderImageLayout = () => {
+  return <div dangerouslySetInnerHTML={{ __html: articleHeaderImage(articleHeaderImageData) }} />;
+};
+
+export const articleHeaderNoImageLayout = () => {
+  return <div dangerouslySetInnerHTML={{ __html: articleHeaderNoImage(articleHeaderNoImageData) }} />;
 };
