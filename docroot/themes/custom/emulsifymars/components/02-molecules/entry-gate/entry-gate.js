8a697bf4723b3bb2188a8485f15ca5e7d532ad72
Mate Nagy <mate_nagy1@epam.com>
2020-08-17 19:33:53 +0200

    AB#181170: using template for entry gate. Refactor js part.

--- docroot/themes/custom/emulsifymars/components/02-molecules/entry-gate/entry-gate.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/entry-gate/entry-gate.js
@@ -2,33 +2,33 @@ import moment from 'moment';
 
 Drupal.behaviors.entryGate = {
   attach(context) {
-    const ageLimit = context.getElementsByClassName('entry-gate')[0].getAttribute('data-age');
     const entryGate = context.getElementsByClassName('entry-gate')[0];
-    const fieldset = context.getElementsByClassName('entry-gate-form__fieldset')[0];
-    const dayInput = context.getElementById('day');
-    const monthInput = context.getElementById('month');
-    const yearInput = context.getElementById('year');
-    const submitBtn = context.getElementsByClassName('entry-gate-form__submit-btn')[0];
-    const errorMessage = context.getElementsByClassName('entry-gate-form__error-message')[0];
-    let givenDateIsValid = true;
+    const ageLimit = entryGate.getAttribute('data-age');
+    const fieldset = entryGate.getElementsByClassName('entry-gate-form__fieldset')[0];
+    const dayInput = document.getElementById('day');
+    const monthInput = document.getElementById('month');
+    const yearInput = document.getElementById('year');
+    const submitBtn = entryGate.getElementsByClassName('entry-gate-form__submit-btn')[0];
+    const errorMessage = entryGate.getElementsByClassName('entry-gate-form__error-message')[0];
 
     // helper for getting cooke with specified name
-    const getCookie = name => {
-      var cookieArr = document.cookie.split(";");
-      for (var i = 0; i < cookieArr.length; i++) {
-        var cookiePair = cookieArr[i].split("=");
-        if (name == cookiePair[0].trim()) {
-          return decodeURIComponent(cookiePair[1]);
+    const getCookieDate = name => {
+      const cookieArr = document.cookie.split(";");
+      for (let i = 0; i < cookieArr.length; i++) {
+        const cookiePair = cookieArr[i].split("=");
+        if (name === cookiePair[0].trim()) {
+          return moment(decodeURIComponent(cookiePair[1]));
         }
       }
       return null;
     }
 
     // compare cookie value against age limit
-    const isOldEnough = (limit) => {
-      if(getCookie('dateOfBirth')) {
-        return (moment(new Date()).diff(getCookie('dateOfBirth'), 'years')) > limit ? true : false;
+    const isOldEnough = (date) => {
+      if (moment.isMoment(date)) {
+        return (moment(new Date()).diff(date, 'years')) >= ageLimit;
       }
+      return false;
     }
 
     // allow only numbers and max 2 characters length
@@ -39,13 +39,15 @@ Drupal.behaviors.entryGate = {
         if (field.value.length > limit) {
           field.value = field.value.subString(0, limit);
         }
-      } else {
+      }
+      else {
         event.preventDefault();
       }
     }
 
-    // display entry gate if cookie is not set or the value of cookie is not enough
-    entryGate.style.display = isOldEnough(ageLimit) ? 'none' : 'flex';
+    // display entry gate if cookie is not set or the value of cookie is not
+    // enough
+    entryGate.style.display = isOldEnough(getCookieDate('dateOfBirth')) ? 'none' : 'flex';
 
     dayInput.addEventListener('keypress', e => checkValueLength(e, dayInput, 2));
     monthInput.addEventListener('keypress', e => checkValueLength(e, dayInput, 2));
@@ -54,23 +56,23 @@ Drupal.behaviors.entryGate = {
     submitBtn.addEventListener('click', event => {
       event.preventDefault();
       const givenDate = moment(`${yearInput.value}-${monthInput.value}-${dayInput.value}`);
-      givenDateIsValid = givenDate.isValid();
-      if (givenDateIsValid === false) {
+
+      if (!givenDate.isValid()) {
         // invalid date is entered
         fieldset.classList.add('entry-gate-form__fieldset--error');
         errorMessage.style.display = 'block';
-      } else {
-        // check age against the age limit
-        if ((moment(new Date()).diff(givenDate, 'years')) >= ageLimit) {
-          // over the age limit, set cookie and hide entry gate
-          context.cookie = `dateOfBirth=${yearInput.value}-${monthInput.value}-${dayInput.value}`;
-          entryGate.style.display = 'none';
-        } else {
-          // under the age limit, show error overlay instead of entry gate
-          entryGate.classList.add('age-error');
-          console.log('error page');
-        }
+        return;
       }
+
+      if (!isOldEnough(givenDate)) {
+        // under the age limit, show error overlay instead of entry gate
+        entryGate.classList.add('age-error');
+        return;
+      }
+
+      // over the age limit, set cookie and hide entry gate
+      context.cookie = `dateOfBirth=${givenDate.format('YYYY-MM-DD')}`;
+      entryGate.style.display = 'none';
     })
   },
 };
