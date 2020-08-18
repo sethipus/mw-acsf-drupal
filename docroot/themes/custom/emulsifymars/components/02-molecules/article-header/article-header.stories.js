b8a15276ce4eaa09cc86e0d444798e2ab6ea5c12
yashcole <oleksii.yashchuk@effem.com>
2020-08-18 08:51:46 +0000

    AB#179332: Article header, fixes after review.

--- docroot/themes/custom/emulsifymars/components/02-molecules/article-header/article-header.stories.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/article-header/article-header.stories.js
@@ -3,13 +3,21 @@ import articleHeaderImage from './article-header-image.twig';
 import articleHeaderImageData from './article-header-image.yml';
 import articleHeaderNoImage from './article-header-noimage.twig';
 import articleHeaderNoImageData from './article-header-noimage.yml';
+import iconsSocial from '../../02-molecules/menus/social/social-menu.yml';
 
 export default { title: 'Molecules/Article Header' };
 
-export const articleHeaderImageLayout = () => {
-  return <div dangerouslySetInnerHTML={{ __html: articleHeaderImage(articleHeaderImageData) }} />;
-};
+export const articleHeaderImageLayout = () => (
+  <div dangerouslySetInnerHTML={{ __html: articleHeaderImage({
+      ...articleHeaderImageData,
+      ...iconsSocial
+    }) }} />
+);
 
-export const articleHeaderNoImageLayout = () => {
-  return <div dangerouslySetInnerHTML={{ __html: articleHeaderNoImage(articleHeaderNoImageData) }} />;
-};
+
+export const articleHeaderNoImageLayout = () => (
+  <div dangerouslySetInnerHTML={{ __html: articleHeaderNoImage({
+      ...articleHeaderNoImageData,
+      ...iconsSocial
+    }) }} />
+);
