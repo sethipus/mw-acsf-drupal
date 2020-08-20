8d04a1c66f4c1abf404428fbf8e715b73fb8d508
zubovsta <stanislau.zubovich@effem.com>
2020-08-20 14:22:53 +0300

    AB#159151: Implementation of Product Detail Hero Module

--- docroot/themes/custom/emulsifymars/components/02-molecules/pdp/pdp-hero/pdp-hero.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/pdp/pdp-hero/pdp-hero.js
@@ -1,10 +1,9 @@
-import Swiper, {Navigation, Pagination} from 'swiper';
-import 'swiper/swiper-bundle.css';
+import Swiper, {Autoplay, Pagination} from 'swiper';
 
 Drupal.behaviors.pdpHero = {
   attach(context) {
     // init swiper
-    Swiper.use([Navigation, Pagination]);
+    Swiper.use([Autoplay, Pagination]);
     const swiper = new Swiper('.pdp-hero-swiper-container', {
       autoplay: true,
       loop: true,
@@ -15,5 +14,23 @@ Drupal.behaviors.pdpHero = {
         type: 'bullets',
       },
     });
+
+    //size control
+    function updateSizeSlider(event, sizeId) {
+      event.preventDefault();
+      let oldSize = document.querySelector(`[data-size-selected="true"]`);
+      let newSize = document.querySelector(`[data-size-id="${sizeId}"]`);
+      let oldSlider = document.querySelector(`[data-slider-size-active="true"]`);
+      let newSlider = document.querySelector(`[data-slider-size-id="${sizeId}"]`);
+      oldSize.dataset.sizeSelected = false;
+      newSize.dataset.sizeSelected = true;
+      oldSlider.dataset.sliderSizeActive = false;
+      newSlider.dataset.sliderSizeActive = true;
+    };
+
+    var sizeElements = document.querySelectorAll('[data-size-id]');
+    sizeElements.forEach((item) => {
+      item.addEventListener('click', e => updateSizeSlider(e, item.dataset.sizeId), false);
+    })
   },
 };
