5639d14441b7b597b1f0ef1d3bce2d1939819341
Andrii Shutov <Andrii_Shutov@epam.com>
2020-08-22 09:05:14 +0300

    AB#177253: Updating color pallete for theme.

--- docroot/themes/custom/emulsifymars/webpack/webpack.common.js
+++ docroot/themes/custom/emulsifymars/webpack/webpack.common.js
@@ -10,12 +10,12 @@ module.exports = {
   entry: {
     svgSprite: path.resolve(webpackDir, 'svgSprite.js'),
     theme_style: path.resolve(webpackDir, 'css.js'),
-    social_feed_slide: path.resolve(distDir + '/js/02-molecules/slide/social-feed-slide', 'social-feed-slide.js'),
-    social_feed: path.resolve(distDir + '/js/02-molecules/social-feed', 'social-feed.js'),
-    media_carousel: path.resolve(distDir + '/js/02-molecules/media-carousel', 'media-carousel.js'),
-    fullscreen_video: path.resolve(distDir + '/js/01-atoms/video/fullscreen-video', 'video.js'),
-    ambient_video: path.resolve(distDir + '/js/01-atoms/video/ambient-video', 'video.js'),
-    entry_gate: path.resolve(distDir + '/js/02-molecules/entry-gate', 'entry-gate.js'),
+    social_feed_slide: path.resolve(`${distDir}/js/02-molecules/slide/social-feed-slide`, 'social-feed-slide.js'),
+    social_feed: path.resolve(`${distDir}/js/02-molecules/social-feed`, 'social-feed.js'),
+    media_carousel: path.resolve(`${distDir}/js/02-molecules/media-carousel`, 'media-carousel.js'),
+    fullscreen_video: path.resolve(`${distDir}/js/01-atoms/video/fullscreen-video`, 'video.js'),
+    ambient_video: path.resolve(`${distDir}/js/01-atoms/video/ambient-video`, 'video.js'),
+    entry_gate: path.resolve(`${distDir}/js/02-molecules/entry-gate`, 'entry-gate.js'),
   },
   module: {
     rules: [loaders.SVGSpriteLoader, loaders.CSSLoader,loaders.SASSLoader, loaders.ImageLoader, loaders.FontLoader],
