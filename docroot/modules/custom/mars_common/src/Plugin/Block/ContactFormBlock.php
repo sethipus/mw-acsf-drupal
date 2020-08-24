24d13f65d04a3dda362f4f92430b2405ceab1e5f
Roland Szuts <Roland_Szuts@epam.com>
2020-08-24 13:54:54 +0200

    AB#181344 [Contact Form] Block, template

---
+++ docroot/modules/custom/mars_common/src/Plugin/Block/ContactFormBlock.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Drupal\mars_common\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a Contact Form block.
+ *
+ * @Block(
+ *   id = "contact_form",
+ *   admin_label = @Translation("Contact Form"),
+ *   category = @Translation("Mars Common")
+ * )
+ */
+class ContactFormBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $conf = $this->getConfiguration();
+
+    $build['#form_id'] = $conf['form_id'] ?? '';
+    $build['#theme'] = 'contact_form_block';
+
+    return $build;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state): array {
+    $form = parent::buildConfigurationForm($form, $form_state);
+
+    $form['form_id'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Form ID'),
+      '#default_value' => $this->configuration['form_id'] ?? '',
+      '#required' => TRUE,
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state): void {
+    parent::blockSubmit($form, $form_state);
+    $this->configuration['form_id'] = $form_state->getValue('form_id');
+  }
+
+}
