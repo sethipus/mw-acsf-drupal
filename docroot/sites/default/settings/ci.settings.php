df192c8b00751f93a91f8a244b8767baa210a917
Mate Nagy <mate_nagy1@epam.com>
2020-09-01 18:56:56 +0200

    AB#181336: adding default LH settings for the CI env.

---
+++ docroot/sites/default/settings/ci.settings.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Settings file for the CI environment.
+ */
+
+/**
+ * Default values for lighthouse API configuration.
+ */
+$settings['mars_lighthouse_defaults']['client_id'] = '<<client_id>>';
+$settings['mars_lighthouse_defaults']['client_secret'] = '<<client_secret>>';
+$settings['mars_lighthouse_defaults']['api_key'] = '<<api_key>>';
+$settings['mars_lighthouse_defaults']['base_path'] = 'https://lighthouse-dev.mars.com';
+$settings['mars_lighthouse_defaults']['subpath'] = '/lh-integration/api/v1';
+$settings['mars_lighthouse_defaults']['port'] = 443;
