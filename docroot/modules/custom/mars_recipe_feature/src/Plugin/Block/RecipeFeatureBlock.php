db3e7840650d3eeeb17aeb141dd81a8db04064bd
yashcole <oleksii.yashchuk@effem.com>
2020-09-08 06:40:36 +0000

    AB#182935: Backend part for recipe feature module.

--- docroot/modules/custom/mars_recipe_feature/src/Plugin/Block/RecipeFeatureBlock.php
+++ docroot/modules/custom/mars_recipe_feature/src/Plugin/Block/RecipeFeatureBlock.php
@@ -7,6 +7,7 @@ use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Plugin\ContextAwarePluginInterface;
+use Drupal\Core\Render\RendererInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 
@@ -28,18 +29,25 @@ use Drupal\Core\Entity\EntityTypeManagerInterface;
 class RecipeFeatureBlock extends BlockBase implements ContextAwarePluginInterface, ContainerFactoryPluginInterface {
 
   /**
-   * EntityTypeManager.
-   *
-   * @var \Drupal\Core\Entity\EntityStorageInterface
-   */
-  protected $entityTypeManager;
-
-  /**
    * NodeStorage.
    *
    * @var \Drupal\Core\Entity\EntityStorageInterface
    */
   protected $nodeStorage;
+
+  /**
+   * Media storage.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $mediaStorage;
+
+  /**
+   * File storage.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $fileStorage;
 
   /**
    * Config Factory.
@@ -49,13 +57,22 @@ class RecipeFeatureBlock extends BlockBase implements ContextAwarePluginInterfac
   protected $configFactory;
 
   /**
+   * The renderer service.
+   *
+   * @var \Drupal\Core\Render\RendererInterface
+   */
+  protected $renderer;
+
+  /**
    * {@inheritdoc}
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, ConfigFactoryInterface $config_factory) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, ConfigFactoryInterface $config_factory, RendererInterface $renderer) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->entityTypeManager = $entity_type_manager;
     $this->configFactory = $config_factory;
     $this->nodeStorage = $entity_type_manager->getStorage('node');
+    $this->mediaStorage = $entity_type_manager->getStorage('media');
+    $this->fileStorage = $entity_type_manager->getStorage('file');
+    $this->renderer = $renderer;
   }
 
   /**
@@ -68,6 +85,7 @@ class RecipeFeatureBlock extends BlockBase implements ContextAwarePluginInterfac
     $plugin_definition,
     $container->get('entity_type.manager'),
     $container->get('config.factory'),
+    $container->get('renderer')
     );
   }
 
@@ -75,19 +93,92 @@ class RecipeFeatureBlock extends BlockBase implements ContextAwarePluginInterfac
    * {@inheritdoc}
    */
   public function build() {
-    $config = $this->getConfiguration();
-    // $theme_settings = $this->configFactory->
-    // get('emulsifymars.settings')->get();
-    $build = [
-      '#eyebrow' => $config['eyebrow'],
-      '#recipe_id_from_url' => $config['take_recipe_id'],
-      '#referenced_recipe' => $config['recipe_id'],
-      '#recipe_media' => $config['recipe_media'],
-      '#cta' => $config['cta'],
-      '#theme' => 'recipe_feature_block',
-    ];
+    global $base_url;
 
-    return $build;
+    $config = $this->getConfiguration();
+    $theme_settings = $this->configFactory->get('emulsifymars.settings')->get();
+
+    $node = $this->getContextValue('node');
+    if (!$node || !$node->bundle() == 'recipes') {
+      $node = $this->nodeStorage->load($this->config['recipe_id']);
+    }
+
+    if (!empty($node)) {
+      // Get brand border.
+      if (!empty($theme_settings['brand_borders']) && count($theme_settings['brand_borders']) > 0) {
+        $border_file = $this->fileStorage->load($theme_settings['brand_borders'][0]);
+        $build['#brand_borders'] = !empty($border_file) ? file_get_contents($base_url . $border_file->createFileUrl()) : '';
+      }
+      $recipe_media_set = [];
+      if (!empty($config['recipe_media'])) {
+
+        $recipe_media = $this->mediaStorage->load($config['recipe_media']);
+        // Manual overriding for media.
+        if ($recipe_media->bundle() == 'lighthouse_video') {
+          $media_file_id = $recipe_media->get('field_media_video_file_1')->target_id;
+          $media_file = $this->fileStorage->load($media_file_id);
+          $media_file_url = !empty($media_file) ? $media_file->createFileUrl() : '';
+          $recipe_media_set = [
+            'video_url' => $media_file_url,
+          ];
+        }
+        elseif ($recipe_media->bundle() == 'lighthouse_image') {
+          $media_file_id = $recipe_media->get('field_media_image')->target_id;
+          $media_file = $this->fileStorage->load($media_file_id);
+          $media_file_url = !empty($media_file) ? $media_file->createFileUrl() : '';
+          $format = '%s 375w, %s 768w, %s 1024w, %s 1440w';
+          $recipe_media_set = [
+            'srcset' => sprintf($format, $media_file_url, $media_file_url, $media_file_url, $media_file_url),
+            'src' => $media_file_url,
+          ];
+        }
+      }
+      else {
+        if (!$node->get('field_recipe_video')->isEmpty()) {
+          // Do it if field_recipe_video field is not empty.
+          // Check bundle.
+          $videoBundle = $node->get('field_recipe_video')->referencedEntities()[0]->bundle();
+          if ($videoBundle == 'video') {
+            // $field_recipe_video = $node->get('field_recipe_video')->view('default');
+            // $field_recipe_video_rendered = $this->renderer->render($field_recipe_video);
+          }
+          else {
+            $media_file_id = $node->get('field_recipe_video')->target_id;
+            $media_file = $this->fileStorage->load($media_file_id);
+            $media_file_url = !empty($media_file) ? $media_file->createFileUrl() : '';
+            $recipe_media_set = [
+              'video_url' => $media_file_url,
+            ];
+          }
+        }
+        else {
+          $media_file_id = $node->get('field_recipe_image')->target_id;
+          $media_file = $this->fileStorage->load($media_file_id);
+          $media_file_url = !empty($media_file) ? $media_file->createFileUrl() : '';
+          $format = '%s 375w, %s 768w, %s 1024w, %s 1440w';
+          $recipe_media_set = [
+            'srcset' => sprintf($format, $media_file_url, $media_file_url, $media_file_url, $media_file_url),
+            'src' => $media_file_url,
+          ];
+        }
+      }
+      $cooking_time = $node->get('field_recipe_cooking_time')->getValue();
+
+      $config['cta']['url'] = $node->toUrl()->toString();
+
+      $build = [
+        '#eyebrow' => $config['eyebrow'],
+        '#recipe_media' => $recipe_media_set,
+        '#cooking_time' => $cooking_time,
+        '#cta' => $config['cta'],
+        '#theme' => 'recipe_feature_block',
+      ];
+      return $build;
+    }
+    else {
+      return [];
+    }
+
   }
 
   /**
@@ -105,11 +196,12 @@ class RecipeFeatureBlock extends BlockBase implements ContextAwarePluginInterfac
       '#default_value' => $config['eyebrow'] ?? '',
     ];
 
-    $form['take_recipe_id'] = [
+    $form['recipe_id_arg'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Take ID from argument'),
+      '#default_value' => $config['recipe_id_arg'] ?? FALSE,
       '#attributes' => [
-        'name' => 'recipe_id_cb',
+        'name' => 'settings[recipe_id_arg]',
       ],
     ];
 
@@ -123,7 +215,7 @@ class RecipeFeatureBlock extends BlockBase implements ContextAwarePluginInterfac
       ],
       '#states' => [
         'visible' => [
-          ':input[name="recipe_id_cb"]' => ['checked' => FALSE],
+          ':input[name="settings[recipe_id_arg]"]' => ['checked' => FALSE],
         ],
       ],
     ];
@@ -132,7 +224,7 @@ class RecipeFeatureBlock extends BlockBase implements ContextAwarePluginInterfac
       '#type' => 'entity_autocomplete',
       '#target_type' => 'media',
       '#title' => $this->t('Recipe Media'),
-      '#default_value' => isset($config['recipe_media']) ? $this->entityTypeManager->getStorage('media')->load($this->configuration['recipe_media']) : NULL,
+      '#default_value' => isset($config['recipe_media']) ? $this->mediaStorage->load($this->configuration['recipe_media']) : NULL,
       '#selection_settings' => ['target_bundles' => ['lighthouse_video', 'lighthouse_image']],
     ];
 
