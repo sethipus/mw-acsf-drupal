bdbc5fc88582c7108ff8b20394c6c1de596c96c5
Anton Tsyhanok <anton.tsyhanok@effem.com>
2020-09-08 18:53:53 +0300

    AB#179336: Recipe body rework.

--- docroot/themes/custom/emulsifymars/components/02-molecules/product-used-module/product-used-module.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/product-used-module/product-used-module.js
@@ -4,10 +4,10 @@ Drupal.behaviors.productUsedCorousel = {
   attach(context) {
     // init swiper
     Swiper.use([Navigation, Pagination, Scrollbar]);
+
     const swiper = new Swiper('.product-used-swiper-container', {
       slidesPerView: 'auto',
       spaceBetween: 20,
-      slidesOffsetBefore: 50,
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
@@ -17,7 +17,6 @@ Drupal.behaviors.productUsedCorousel = {
       },
       breakpoints: {
         1440: {
-          watchOverflow: 'false',
           direction: 'vertical'
         }
       }
