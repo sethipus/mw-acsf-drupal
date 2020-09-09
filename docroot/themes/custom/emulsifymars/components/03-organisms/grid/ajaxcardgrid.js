d50356eb8fc350ed353c2d2cbf3cabc1059bd23b
Roland Szuts <Roland_Szuts@epam.com>
2020-09-09 15:26:22 +0200

    AB#182944 Scroll theming

--- docroot/themes/custom/emulsifymars/components/03-organisms/grid/ajaxcardgrid.js
+++ docroot/themes/custom/emulsifymars/components/03-organisms/grid/ajaxcardgrid.js
@@ -3,9 +3,9 @@ Drupal.behaviors.ajaxcardgrid = {
 
     const seeMoreBtn = document.querySelector('.ajax-card-grid__more-link .default-link');
     seeMoreBtn.addEventListener('click', (event) => {
-      event.preventDefault();
-      jQuery('.js-pager__items.pager li a.button').trigger('click');
-    });
+        event.preventDefault();
+      }
+    );
 
   },
 };
