ebbd7b630bdfdb8e5877fa6798e59b53f4fdc35c
Roland Szuts <Roland_Szuts@epam.com>
2020-09-09 11:04:24 +0200

    AB#182944 Ajax loader for cards

--- docroot/themes/custom/emulsifymars/components/03-organisms/grid/ajaxcardgrid.js
+++ docroot/themes/custom/emulsifymars/components/03-organisms/grid/ajaxcardgrid.js
@@ -1,10 +1,9 @@
 Drupal.behaviors.ajaxcardgrid = {
   attach(context) {
+
     const seeMoreBtn = document.querySelector('.ajax-card-grid__more-link .default-link');
     seeMoreBtn.addEventListener('click', (event) => {
       event.preventDefault();
-      console.log('ajax request triggered');
-
       jQuery('.js-pager__items.pager li a.button').trigger('click');
     });
 
