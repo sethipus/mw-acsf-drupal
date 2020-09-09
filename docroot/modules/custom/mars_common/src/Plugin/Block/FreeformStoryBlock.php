f20141aeeec8ef8ec1a758525cfb09f4c3e5d76d
Artem Sosiedko <artem_sosiedko@epam.com>
2020-09-09 17:26:52 +0300

    AB#182957: Freeform Story Block Module.

--- docroot/modules/custom/mars_common/src/Plugin/Block/FreeformStoryBlock.php
+++ docroot/modules/custom/mars_common/src/Plugin/Block/FreeformStoryBlock.php
@@ -4,6 +4,7 @@ namespace Drupal\mars_common\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\mars_common\Traits\EntityBrowserFormTrait;
 
 /**
  * Provides a 'MARS: Freeform Story Block' Block.
@@ -16,6 +17,28 @@ use Drupal\Core\Form\FormStateInterface;
  */
 class FreeformStoryBlock extends BlockBase {
 
+  use EntityBrowserFormTrait;
+
+  /**
+   * Lighthouse entity browser id.
+   */
+  const LIGHTHOUSE_ENTITY_BROWSER_ID = 'image_browser';
+
+  /**
+   * Aligned by left side.
+   */
+  const LEFT_ALIGNED = 'left_aligned';
+
+  /**
+   * Aligned by right side.
+   */
+  const RIGHT_ALIGNED = 'right_aligned';
+
+  /**
+   * Aligned by center.
+   */
+  const CENTER_ALIGNED = 'center_aligned';
+
   /**
    * {@inheritdoc}
    */
@@ -27,26 +50,36 @@ class FreeformStoryBlock extends BlockBase {
       '#title' => $this->t('Block aligned'),
       '#default_value' => $this->configuration['block_aligned'],
       '#options' => [
-        'left_aligned' => $this->t('Left aligned'),
-        'right_aligned' => $this->t('Right aligned'),
-        'center_aligned' => $this->t('Center aligned'),
+        self::LEFT_ALIGNED => $this->t('Left aligned'),
+        self::RIGHT_ALIGNED => $this->t('Right aligned'),
+        self::CENTER_ALIGNED => $this->t('Center aligned'),
       ],
     ];
     $form['header_1'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Header 1'),
       '#default_value' => $this->configuration['header_1'],
+      '#maxlength' => 60,
       '#required' => TRUE,
     ];
     $form['header_2'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Header 2'),
       '#default_value' => $this->configuration['header_2'],
+      '#maxlength' => 60,
     ];
+    // Entity Browser element for background image.
+    $form['image'] = $this->getEntityBrowserForm(self::LIGHTHOUSE_ENTITY_BROWSER_ID, $this->configuration['image'], 1, 'thumbnail');
+    // Convert the wrapping container to a details element.
+    $form['image']['#type'] = 'details';
+    $form['image']['#title'] = $this->t('Image');
+    $form['image']['#open'] = TRUE;
+
     $form['body'] = [
-      '#type' => 'body',
+      '#type' => 'text_format',
       '#title' => $this->t('Description'),
-      '#default_value' => $this->configuration['body'],
+      '#default_value' => $this->configuration['body']['value'] ?? '',
+      '#format' => $this->configuration['body']['format'] ?? 'rich_text',
       '#maxlength' => 1000,
       '#required' => TRUE,
     ];
@@ -66,8 +99,12 @@ class FreeformStoryBlock extends BlockBase {
     $build['#block_aligned'] = $this->configuration['block_aligned'];
     $build['#header_1'] = $this->configuration['header_1'];
     $build['#header_2'] = $this->configuration['header_2'];
-    $build['#body'] = $this->configuration['body'];
+    $build['#body'] = $this->configuration['body']['value'];
     $build['#background_shape'] = $this->configuration['background_shape'];
+    /** @var \Drupal\media\MediaInterface $media */
+    if (!empty($this->configuration['image']) && $media = static::loadEntityBrowserEntity($this->configuration['image'])) {
+      $build['#image'] = file_create_url($media->getFileUri());
+    }
     $build['#theme'] = 'freeform_story_block';
 
     return $build;
@@ -83,6 +120,7 @@ class FreeformStoryBlock extends BlockBase {
    */
   public function blockSubmit($form, FormStateInterface $form_state) {
     $this->setConfiguration($form_state->getValues());
+    $this->configuration['image'] = $this->getEntityBrowserValue($form_state, 'image');
   }
 
   /**
@@ -94,8 +132,9 @@ class FreeformStoryBlock extends BlockBase {
       'block_aligned' => $config['block_aligned'] ?? '',
       'header_1' => $config['header_1'] ?? $this->t('Header 1'),
       'header_2' => $config['header_2'] ?? '',
-      'body' => $config['body'] ?? '',
+      'body' => $config['body']['value'] ?? '',
       'background_shape' => $config['background_shape'] ?? '',
+      'image' => $config['image'] ?? '',
     ];
   }
 
