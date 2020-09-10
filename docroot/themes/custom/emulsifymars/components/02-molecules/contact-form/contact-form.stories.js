b8c31de9f90940dad53cf6680a4e18a6b635cb12
Peter Bartos <Peter_Bartos@epam.com>
2020-09-10 15:43:29 +0200

    AB#181343: Changed rem calculation from 16 to 10px for our drupal settings - corrected stories js

--- docroot/themes/custom/emulsifymars/components/02-molecules/contact-form/contact-form.stories.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/contact-form/contact-form.stories.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import contactForm from './contact-form.twig';
-import { useEffect } from '@storybook/client-api';
-
-import './contact-form';
 
 export default { title: 'Molecules/Contact Form' };
 
 export const contactFormModule = () => {
-  useEffect(() => Drupal.attachBehaviors(), []);
   return <div dangerouslySetInnerHTML={{ __html: contactForm() }} />
 };
