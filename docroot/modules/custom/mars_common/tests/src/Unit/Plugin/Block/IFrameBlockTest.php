c7a8e886ebb3c4ba8af38ef13ffc41c27053d2da
Roland Szuts <Roland_Szuts@epam.com>
2020-09-10 15:44:38 +0200

    AB#184887: IFrame unit tests

---
+++ docroot/modules/custom/mars_common/tests/src/Unit/Plugin/Block/IFrameBlockTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Drupal\Tests\mars_common\Unit\Plugin\Block;
+
+use Drupal;
+use Drupal\mars_common\Plugin\Block\IFrameBlock;
+use Drupal\Tests\UnitTestCase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class IFrameBlockTest.
+ *
+ * @package Drupal\Tests\mars_common\Unit
+ * @covers \Drupal\mars_common\Plugin\Block\ContactFormBlock
+ */
+class IFrameBlockTest extends UnitTestCase {
+
+  /**
+   * Mock.
+   *
+   * @var \Symfony\Component\DependencyInjection\ContainerInterface|\PHPUnit\Framework\MockObject\MockObject
+   */
+  private $containerMock;
+
+  /**
+   * Tested IFrameBlock block.
+   *
+   * @var \Drupal\mars_common\Plugin\Block\IFrameBlock
+   */
+  private $iFrameBlock;
+
+  /**
+   * Test block configuration.
+   *
+   * @var array
+   */
+  private $configuration = [
+    'url' => 'demo-url',
+    'accessibility_title' => 'Accessibility Title',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->createMocks();
+    Drupal::setContainer($this->containerMock);
+
+    $definitions = [
+      'provider'    => 'test',
+      'admin_label' => 'test',
+    ];
+
+    $this->iFrameBlock = new IFrameBlock(
+      $this->configuration,
+      'iframe_block',
+      $definitions
+    );
+  }
+
+  /**
+   * Create all mocks for tests.
+   */
+  private function createMocks(): void {
+    $this->containerMock = $this->createMock(ContainerInterface::class);
+    $this->formStateMock = $this->createMock(FormStateInterface::class);
+  }
+
+  /**
+   * Test building block.
+   *
+   * @test
+   */
+  public function buildBlockRenderArrayProperly() {
+    $build = $this->iFrameBlock->build();
+
+    $this->assertCount(3, $build);
+    $this->assertArrayHasKey('#url', $build);
+    $this->assertEquals($this->configuration['url'], $build['#url']);
+    $this->assertArrayHasKey('#accessibility_title', $build);
+    $this->assertEquals($this->configuration['accessibility_title'], $build['#accessibility_title']);
+    $this->assertArrayHasKey('#theme', $build);
+    $this->assertEquals('iframe_block', $build['#theme']);
+  }
+
+}
