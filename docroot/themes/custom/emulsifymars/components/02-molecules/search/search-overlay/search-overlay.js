4f503587572ac50657aafc681993f32a35db4fff
yashcole <oleksii.yashchuk@effem.com>
2020-09-11 19:55:24 +0000

    AB#182949: Make some UI changes, refactored.

--- docroot/themes/custom/emulsifymars/components/02-molecules/search/search-overlay/search-overlay.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/search/search-overlay/search-overlay.js
@@ -3,7 +3,9 @@ Drupal.behaviors.searchOverlay = {
     $('.inline-search').click(function () {
       $('.inline-search--closebtn').toggleClass('inline-search--hidden');
       $('.inline-search--searchbtn').toggleClass('inline-search--hidden');
-      $('.search-autocomplete-wrapper').slideToggle(250);
+     // $('.search-autocomplete-wrapper').toggleClass('opened').slideToggle(250);
+      $('.search-autocomplete-wrapper').toggleClass('opened suggested').slideToggle(250);
+      return false;
     });
   }
 };
