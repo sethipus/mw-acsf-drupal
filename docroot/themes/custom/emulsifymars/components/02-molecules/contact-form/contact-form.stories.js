9fd0fa1553d2b790d520509ded9fef1eb1bbb6a9
Peter Bartos <Peter_Bartos@epam.com>
2020-09-11 10:47:03 +0200

    AB#181343: Modified css with additional queries and removed storybook files as component cannot be seen there

--- docroot/themes/custom/emulsifymars/components/02-molecules/contact-form/contact-form.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import contactForm from './contact-form.twig';
-
-export default { title: 'Molecules/Contact Form' };
-
-export const contactFormModule = () => {
-  return <div dangerouslySetInnerHTML={{ __html: contactForm() }} />
-};
