4f2814492ca1a791d29bef659c16e66a898f5842
Roland Szuts <Roland_Szuts@epam.com>
2020-09-15 13:07:02 +0200

    AB#184886: Contact Help Banner unit tests

---
+++ docroot/modules/custom/mars_common/tests/src/Unit/Plugin/Block/ContactHelpBannerBlockTest.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace Drupal\Tests\mars_common\Unit\Plugin\Block;
+
+use Drupal;
+use Drupal\mars_common\Plugin\Block\ContactHelpBannerBlock;
+use Drupal\mars_common\ThemeConfiguratorParser;
+use Drupal\Tests\UnitTestCase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Class ContactHelpBannerBlockTest.
+ *
+ * @package Drupal\Tests\mars_common\Unit
+ * @covers \Drupal\mars_common\Plugin\Block\ContactFormBlock
+ */
+class ContactHelpBannerBlockTest extends UnitTestCase {
+
+  /**
+   * Mock.
+   *
+   * @var \Symfony\Component\DependencyInjection\ContainerInterface|\PHPUnit\Framework\MockObject\MockObject
+   */
+  private $containerMock;
+
+  /**
+   * Tested ContactHelpBannerBlock block.
+   *
+   * @var \Drupal\mars_common\Plugin\Block\ContactHelpBannerBlock
+   */
+  private $contactHelpBannerBlock;
+
+  /**
+   * ThemeConfiguratorParserMock.
+   *
+   * @var \PHPUnit\Framework\MockObject\MockObject|\Drupal\mars_common\ThemeConfiguratorParser
+   */
+  protected $themeConfiguratorParserMock;
+
+  /**
+   * Test block configuration.
+   *
+   * @var array
+   */
+  private $configuration = [
+    'label' => 'Contact Help',
+    'label_display' => FALSE,
+    'description' => 'Description',
+    'social_links_label' => 'See More On',
+    'phone_cta_label' => 'Phone',
+    'phone_cta_number' => '123456789',
+    'email_cta_label' => 'Email Us',
+    'email_cta_address' => 'support@mars.com',
+    'help_and_contact_cta_label' => 'Help and Contact',
+    'help_and_contact_cta_url' => '/help',
+    'theme' => 'contact_help_banner_block',
+  ];
+
+  /**
+   * Brand shape from theme configurator.
+   *
+   * @var string
+   */
+  private $brandShapeSvg = '<svg width="100" height="100"><circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" /></svg>';
+
+  /**
+   * Social links from theme configurator.
+   *
+   * @var \string[][]
+   */
+  private $socialLinks = [
+    [
+      'title' => 'facebook',
+      'url' => 'https://facebook.com',
+      'icon' => 'facebook.png',
+    ],
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->createMocks();
+    Drupal::setContainer($this->containerMock);
+
+    $definitions = [
+      'provider' => 'test',
+      'admin_label' => 'test',
+    ];
+
+    $this->contactHelpBannerBlock = new ContactHelpBannerBlock(
+      $this->configuration,
+      'contact_help_banner_block',
+      $definitions,
+      $this->themeConfiguratorParserMock
+    );
+  }
+
+  /**
+   * Create all mocks for tests.
+   */
+  private function createMocks(): void {
+    $this->containerMock = $this->createMock(ContainerInterface::class);
+    $this->themeConfiguratorParserMock = $this->createMock(ThemeConfiguratorParser::class);
+  }
+
+  /**
+   * Test building block.
+   *
+   * @test
+   */
+  public function buildBlockRenderArrayProperly() {
+    $this->themeConfiguratorParserMock
+      ->expects($this->any())
+      ->method('getFileContentFromTheme')
+      ->withConsecutive(['brand_shape'])
+      ->willReturn($this->brandShapeSvg);
+
+    $this->themeConfiguratorParserMock
+      ->expects($this->any())
+      ->method('socialLinks')
+      ->willReturn($this->socialLinks);
+
+    $build = $this->contactHelpBannerBlock->build();
+
+    $this->assertCount(12, $build);
+    $this->assertEquals($this->configuration['label'], $build['#label']);
+    $this->assertEquals($this->configuration['description'], $build['#description']);
+    $this->assertEquals($this->configuration['social_links_label'], $build['#social_links_label']);
+    $this->assertEquals($this->configuration['phone_cta_label'], $build['#phone_cta_label']);
+    $this->assertEquals('tel:' . $this->configuration['phone_cta_number'], $build['#phone_cta_link']);
+    $this->assertEquals($this->configuration['email_cta_label'], $build['#email_cta_label']);
+    $this->assertEquals('mailto:' . $this->configuration['email_cta_address'], $build['#email_cta_link']);
+    $this->assertEquals($this->configuration['help_and_contact_cta_label'], $build['#help_and_contact_cta_label']);
+    $this->assertEquals($this->configuration['help_and_contact_cta_url'], $build['#help_and_contact_cta_url']);
+    $this->assertEquals($this->socialLinks, $build['#social_menu_items']);
+    $this->assertEquals($this->brandShapeSvg, $build['#brand_shape']);
+    $this->assertEquals($this->configuration['theme'], $build['#theme']);
+  }
+
+}
