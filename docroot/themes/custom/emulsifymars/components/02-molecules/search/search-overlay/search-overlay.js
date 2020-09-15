70f435236afd04e063770b7a5a667f99e7dce3a9
yashcole <oleksii.yashchuk@effem.com>
2020-09-15 10:11:44 +0000

    AB#182949: Fixed js to work with drupal site.

--- docroot/themes/custom/emulsifymars/components/02-molecules/search/search-overlay/search-overlay.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/search/search-overlay/search-overlay.js
@@ -1,30 +1,30 @@
-Drupal.behaviors.searchOverlay = {
-  attach(context) {
-      $('.inline-search').click(function () {
-      $('.search-autocomplete-wrapper:visible').slideUp(250, function (){
-        $(this).removeClass('opened');
-      });
-      $('.search-autocomplete-wrapper:hidden').slideDown(250, function (){
-        $(this).addClass('opened');
-      });
-      return false;
-    });
-
-    $('.header__menu--secondary-mobile .inline-search').click(function (){
-      $(this).hide();
-      $('.search-autocomplete-wrapper-mobile').slideDown(250, function (){
-        $(this).addClass('opened');
-      });
-    });
-
-    $(document).click(function(event){
-      if ($(event.target).parents('.search-autocomplete-wrapper-mobile').length == 0) {
-        $('.header__menu--secondary-mobile .inline-search').show();
-        $('.search-autocomplete-wrapper-mobile:visible').slideUp(250, function (){
+(function ($) {
+  Drupal.behaviors.searchOverlay = {
+    attach(context) {
+       $('.inline-search').click(function () {
+        $('.search-autocomplete-wrapper:visible').slideUp(250, function (){
           $(this).removeClass('opened');
         });
-      }
-    });
+        $('.search-autocomplete-wrapper:hidden').addClass('opened').slideDown(250);
+        return false;
+      });
 
-  }
-};
+      $('.header__menu--secondary-mobile .inline-search').click(function (){
+        $(this).hide();
+        $('.search-autocomplete-wrapper-mobile').slideDown(250, function (){
+          $(this).addClass('opened');
+        });
+      });
+
+      $(document).click(function(event){
+        if ($(event.target).parents('.search-autocomplete-wrapper-mobile').length == 0) {
+          $('.header__menu--secondary-mobile .inline-search').show();
+          $('.search-autocomplete-wrapper-mobile:visible').slideUp(250, function (){
+            $(this).removeClass('opened');
+          });
+        }
+      });
+
+    }
+  };
+})(jQuery);
