db2d4bf09a7fd8de989ece234fd5ce714d1a01c9
Anton Tsyhanok <anton.tsyhanok@effem.com>
2020-09-15 16:45:43 +0300

    AB#182952  Update styles for Search Header Results

--- docroot/themes/custom/emulsifymars/components/01-atoms/search-results-item/search-results-item.stories.js
+++ docroot/themes/custom/emulsifymars/components/01-atoms/search-results-item/search-results-item.stories.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useEffect } from '@storybook/client-api';
 import searchResultsItem from './search-results-item.twig';
 import searchResultsItemData from './search-results-item.yml';
+import './search-results-item';
 
 export default { title: 'Atoms/Search results item' };
 
-export const searchResultsItemModule = () => (
-  <div dangerouslySetInnerHTML={{ __html: searchResultsItem(searchResultsItemData) }} />
-);
+export const searchResultsItemModule = () => {
+  useEffect(() => Drupal.attachBehaviors(), []);
+  return <div dangerouslySetInnerHTML={{__html: searchResultsItem(searchResultsItemData)}}/>
+}
--- docroot/themes/custom/emulsifymars/components/03-organisms/search-results/search-results-all-results/all-results.stories.js
+++ docroot/themes/custom/emulsifymars/components/03-organisms/search-results/search-results-all-results/all-results.stories.js
@@ -5,16 +5,19 @@ import allResultsData from './all-results.yml';
 import ajaxCardGridData from '../../grid/ajax-card-grid.yml';
 import searchResultsData from '../../../02-molecules/search/search-results/search-results.yml';
 
+import { useEffect } from '@storybook/client-api';
+import '../../../01-atoms/search-results-item/search-results-item';
+
 /**
  * Storybook Definition.
  */
 export default { title: 'Organisms/Search Results/View all results' };
 
-export const allResultsTemplate = () => (
-  <div dangerouslySetInnerHTML={{ __html: allResultsTwig({
+export const allResultsTemplate = () => {
+  useEffect(() => Drupal.attachBehaviors(), []);
+  return <div dangerouslySetInnerHTML={{ __html: allResultsTwig({
       ...allResultsData,
       ...ajaxCardGridData,
       ...searchResultsData
   }) }} />
-);
-
+};
