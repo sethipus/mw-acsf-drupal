cd9ec5082d1568f47737ead39f035b2990950fa7
Andrii Shutov <Andrii_Shutov@epam.com>
2020-09-15 17:26:45 +0300

    AB#182951: fixes for search header block.

--- docroot/themes/custom/emulsifymars/components/03-organisms/search-results/search-results-all-results/all-results.stories.js
+++ docroot/themes/custom/emulsifymars/components/03-organisms/search-results/search-results-all-results/all-results.stories.js
@@ -4,17 +4,35 @@ import allResultsTwig from './all-results.twig';
 import allResultsData from './all-results.yml';
 import ajaxCardGridData from '../../grid/ajax-card-grid.yml';
 import searchResultsData from '../../../02-molecules/search/search-results/search-results.yml';
+import productCard from './../../../02-molecules/card/product-card/product-card.twig';
+import productCardData from './../../../02-molecules/card/product-card/product-card.yml';
+import recipeCard from './../../../02-molecules/card/recipe-card/recipe-card.twig';
+import recipeCardData from './../../../02-molecules/card/recipe-card/recipe-card.yml';
+import articleCard from './../../../02-molecules/card/article-card/article-card.twig';
+import articleCardData from './../../../02-molecules/card/article-card/article-card.yml';
 
 /**
  * Storybook Definition.
  */
 export default { title: 'Organisms/Search Results/View all results' };
 
-export const allResultsTemplate = () => (
-  <div dangerouslySetInnerHTML={{ __html: allResultsTwig({
+export const allResultsTemplate = () => {
+  ajaxGridData.items = [
+    productCard(productCardData),
+    productCard(productCardData),
+    recipeCard(recipeCardData),
+    articleCard(articleCardData),
+    productCard(productCardData),
+    recipeCard(recipeCardData),
+    articleCard(articleCardData),
+    productCard(productCardData)
+  ];
+
+  return <div dangerouslySetInnerHTML={{ __html: allResultsTwig({
       ...allResultsData,
       ...ajaxCardGridData,
       ...searchResultsData
-  }) }} />
-);
+    }) 
+  }} />
+};
 
