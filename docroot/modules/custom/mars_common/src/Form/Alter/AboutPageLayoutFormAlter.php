3135a0b3df4d8e2e65cd1a45dfac75ac79810d9f
yashcole <oleksii.yashchuk@effem.com>
2020-09-22 13:19:49 +0000

    AB#183069: Added classes and layouts.

---
+++ docroot/modules/custom/mars_common/src/Form/Alter/AboutPageLayoutFormAlter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drupal\mars_common\Form\Alter;
+
+/**
+ * Class HomepageLayoutFormAlter.
+ *
+ * @package Drupal\mars_common\Form\Alter
+ */
+class AboutPageLayoutFormAlter extends LayoutFormAlterBase {
+
+  const FIXED_SECTIONS = [
+    'about_page_introduction',
+    'about_page_storytelling',
+    'about_page_contact',
+  ];
+
+}
