744abe0b8906a4d5882dceecabc5ea831224b318
yashcole <oleksii.yashchuk@effem.com>
2020-09-24 09:10:03 +0000

    AB#163267: Added form alter class.

---
+++ docroot/modules/custom/mars_common/src/Form/Alter/ContentHubPageLayoutFormAlter.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\mars_common\Form\Alter;
+
+/**
+ * Class ContentHubPageLayoutFormAlter.
+ *
+ * @package Drupal\mars_common\Form\Alter
+ */
+class ContentHubPageLayoutFormAlter extends LayoutFormAlterBase {
+
+  const FIXED_SECTIONS = [
+    'content_hub_parent_header',
+    'content_hub_card_grid_w_filters',
+  ];
+
+}
