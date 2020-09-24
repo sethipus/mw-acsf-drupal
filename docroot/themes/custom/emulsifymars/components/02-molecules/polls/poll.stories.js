ee6f481664b4bfeccd3784931fdb821f8830f469
Szilveszter Marta <Szilveszter_Marta@epam.com>
2020-09-24 12:10:29 +0200

    AB#185954: polls module FE fixes

--- docroot/themes/custom/emulsifymars/components/02-molecules/polls/poll.stories.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/polls/poll.stories.js
@@ -3,12 +3,14 @@ import React from 'react';
 import poll from './poll-vote.twig';
 import pollSubmitted from './poll-results.twig';
 import pollData from './poll.yml';
+import defaultLink from './../../01-atoms/links/defaultLink/defaultLink.twig'
 
 export default { title: 'Molecules/Polling' };
 
-export const pollingInitialState = () => (
-  <div style={{padding: '2rem'}} dangerouslySetInnerHTML={{ __html: poll(pollData) }} />
-);
+export const pollingInitialState = () => {
+  pollData.vote_button = defaultLink({default_link_content: 'Submit'});
+  return <div style={{padding: '2rem'}} dangerouslySetInnerHTML={{ __html: poll(pollData) }} />;
+};
 
 export const pollingVoteSubmitted = () => (
   <div style={{padding: '2rem'}} dangerouslySetInnerHTML={{ __html: pollSubmitted(pollData) }} />
