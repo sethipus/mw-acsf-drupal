30baa25c4c33705f6d2dbf163826d0c97ceb08d0
Roland Szuts <Roland_Szuts@epam.com>
2020-09-29 13:40:54 +0200

    AB#186195: Init once button-vote for feedback

--- docroot/themes/custom/emulsifymars/components/02-molecules/feedback-module/feedback.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/feedback-module/feedback.js
@@ -1,15 +1,13 @@
 (function($){
   Drupal.behaviors.feedback = {
     attach(context) {
-      $('.feedback-module').each(function() {
-        let radioBtn = $(this).find('.feedback-module__radio:not(.inited)');
+      $('.feedback-module').once('feedback').each(function() {
+        let radioBtn = $(this).find('.feedback-module__radio');
         let submitBtn = $(this).find('.button-vote');
-        radioBtn.each(function () {
-          $(this)
-            .change(() => {
-              submitBtn.mousedown();
-            })
-            .addClass('inited');
+        radioBtn.each(function() {
+          $(this).change(() => {
+            submitBtn.mousedown();
+          });
         });
       });
     },
