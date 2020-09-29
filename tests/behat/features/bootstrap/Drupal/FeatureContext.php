fc3b1c60407388e1b3fb50099312135ccd869b7a
Igor_Gaiduk <Igor_Gaiduk@epam.com>
2020-09-29 17:19:20 +0300

    added list of files

--- tests/behat/features/bootstrap/Drupal/FeatureContext.php
+++ tests/behat/features/bootstrap/Drupal/FeatureContext.php
@@ -43,6 +43,7 @@ class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext
     public function loginDrupal()
     {
         echo "pwd: " . shell_exec('pwd') . "\n";
+        echo "list of files: " . shell_exec('cd /home/vsts/work/1/s/vendor/drush/drush; ls') . "\n";
         $loginUrl = preg_replace('/\n$/', '', shell_exec('cd /home/vsts/work/1/s/vendor/drush/drush; drush uli'));
         echo "loginUrl: " . $loginUrl . "\n";
         $loginUrl = str_replace( 'https://mars.ddev.site:8443', 'http://mars.ddev.site:8080', $loginUrl );
