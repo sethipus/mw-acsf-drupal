c0cb5821b0e41cb067ad51920e5f3281af841967
Igor_Gaiduk <Igor_Gaiduk@epam.com>
2020-09-29 14:20:09 +0300

    Moved to drush directory when using drush

--- tests/behat/features/bootstrap/Drupal/FeatureContext.php
+++ tests/behat/features/bootstrap/Drupal/FeatureContext.php
@@ -42,9 +42,7 @@ class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext
      */
     public function loginDrupal()
     {
-        echo "pwd: " . shell_exec('pwd') . "\n";
-        echo "cd ../vendor/bin; ls: " . shell_exec('cd ../vendor/bin; ls') . "\n";
-        $loginUrl = preg_replace('/\n$/', '', shell_exec('cd ../vendor/bin; drush uli'));
+        $loginUrl = preg_replace('/\n$/', '', shell_exec('cd /home/vsts/work/1/s/vendor/bin; drush uli'));
         echo "loginUrl: " . $loginUrl . "\n";
         $loginUrl = str_replace( 'https://mars.ddev.site:8443', 'http://mars.ddev.site:8080', $loginUrl );
         echo "http loginUrl: " . $loginUrl;
