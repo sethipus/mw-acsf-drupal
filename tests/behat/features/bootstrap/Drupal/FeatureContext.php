ea697ba1eb1040bc43dfae1a3920caee9fac6618
Igor_Gaiduk <Igor_Gaiduk@epam.com>
2020-09-30 13:43:31 +0300

    Added cd vendor/bin & drush

--- tests/behat/features/bootstrap/Drupal/FeatureContext.php
+++ tests/behat/features/bootstrap/Drupal/FeatureContext.php
@@ -43,12 +43,7 @@ class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext
     public function loginDrupal()
     {
         echo "pwd: " . shell_exec('pwd') . "\n";
-
-
-        echo "drush status: " . shell_exec('drush status') . "\n";
-        echo "cd and drush status: " . shell_exec('cd /home/vsts/work/1/s/vendor/drush/drush; drush status') . "\n";
-
-        $loginUrl = preg_replace('/\n$/', '', shell_exec('cd /home/vsts/work/1/s/vendor/drush/drush; drush uli'));
+        $loginUrl = preg_replace('/\n$/', '', shell_exec('cd /vendor/bin; drush uli'));
         echo "loginUrl: " . $loginUrl . "\n";
         $loginUrl = str_replace( 'https://mars.ddev.site:8443', 'http://mars.ddev.site:8080', $loginUrl );
         echo "http loginUrl: " . $loginUrl;
