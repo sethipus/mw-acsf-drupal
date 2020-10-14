f807835328f661f772911c83a1fb973224648764
Anton Tsyhanok <anton.tsyhanok@effem.com>
2020-10-14 08:16:13 +0300

    AB#191879: Add clear button for filters.

--- docroot/themes/custom/emulsifymars/components/02-molecules/product-hub-search-filter/product-hub-search-filter.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/product-hub-search-filter/product-hub-search-filter.js
@@ -20,6 +20,10 @@ Drupal.behaviors.searchFilterBehaviour = {
           break;
         case target.classList.contains('checkbox-item__input'):
           updateCounters();
+        case target.classList.contains('search-filter-info__applied-clear'):
+          const currentFilter = context.querySelector('#' + target.getAttribute('data-id'));
+          currentFilter.checked = false;
+          updateCounters();
           break;
       }
     });
@@ -40,7 +44,7 @@ Drupal.behaviors.searchFilterBehaviour = {
         else {
           document.location.search = '';
         }
-        
+
       });
     });
 
@@ -55,11 +59,11 @@ Drupal.behaviors.searchFilterBehaviour = {
         if (searchQuery !== '') {
           queryElements.push('search=' + searchQuery);
         }
-  
+
         filterBlocks.forEach(function(element) {
           const inputLabels = element.querySelectorAll('.checkbox-item__input:checked + label');
           const inputElements = element.querySelectorAll('.checkbox-item__input:checked');
-  
+
           inputLabels.forEach(function(label) {
             appliedFilters.push(label.innerText);
           });
@@ -76,7 +80,8 @@ Drupal.behaviors.searchFilterBehaviour = {
     });
 
     const updateCounters = () => {
-      let appliedFilters = [];
+      let appliedFilters = '';
+      let appliedFiltersCounter = 0;
       const filterBlocks = context.querySelectorAll('.filter-block');
       const appliedFiltersBlock = context.querySelector('.search-filter-info__applied');
       const appliedFiltersCount = context.querySelector('.search-filter-info__applied-count');
@@ -89,7 +94,13 @@ Drupal.behaviors.searchFilterBehaviour = {
         let counter = inputLabels.length;
         counterElement.innerHTML = counter ? counter : '';
         inputLabels.forEach(function(label) {
-          appliedFilters.push(label.innerText);
+          appliedFilters += '\
+            <span class="search-filter-info__applied-name">\
+              <span>'+ label.innerText +'</span>\
+              <div data-id="'+ label.getAttribute('for') +'" class="search-filter-info__applied-clear"></div>\
+            </span>\
+            '
+          appliedFiltersCounter++;
         });
       });
 
@@ -101,8 +112,8 @@ Drupal.behaviors.searchFilterBehaviour = {
         clearAllButton.classList.add('search-filter-block__button--hidden');
       }
 
-      appliedFiltersCount.innerHTML = appliedFilters.length;
-      appliedFiltersList.innerHTML = appliedFilters.join(', ');
+      appliedFiltersCount.innerHTML = appliedFiltersCounter;
+      appliedFiltersList.innerHTML = appliedFilters;
     }
   },
 };
