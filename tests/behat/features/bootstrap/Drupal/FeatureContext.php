d239cdd889a950ed1d189935fc9f25630e25147b
Igor_Gaiduk <Igor_Gaiduk@epam.com>
2020-10-19 16:12:55 +0300

    added uri

--- tests/behat/features/bootstrap/Drupal/FeatureContext.php
+++ tests/behat/features/bootstrap/Drupal/FeatureContext.php
@@ -75,7 +75,7 @@ class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext
 
 
         //$loginUrl = preg_replace('/\n$/', '', shell_exec('cd /home/vsts/.composer/vendor/bin; drush uli'));
-        $loginUrl = preg_replace('/\n$/', '', shell_exec('cd ../vendor/bin; ./drush uli'));
+        $loginUrl = preg_replace('/\n$/', '', shell_exec('cd ../vendor/bin; ./drush uli --uri=http://mars.ddev.site:8080'));
         echo "loginUrl: " . $loginUrl . "\n";
         $loginUrl = str_replace('https://mars.ddev.site:8443', 'http://mars.ddev.site:8080', $loginUrl);
         echo "http loginUrl: " . $loginUrl;
