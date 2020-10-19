9640e9744f0b8fe8fcfdb6de60d211e6167c60e2
zubovsta <stanislau.zubovich@effem.com>
2020-10-19 16:22:07 +0300

    AB#184570 - [FE] No parallax effect added for nutrition part (no mobile scroll-snap)

--- docroot/themes/custom/emulsifymars/components/03-organisms/pdp-body/pdp-body.js
+++ docroot/themes/custom/emulsifymars/components/03-organisms/pdp-body/pdp-body.js
@@ -1,16 +1,185 @@
 import Swiper, {Autoplay, Pagination} from 'swiper';
-import SnapScroll from 'snapscroll';
 
 (function($) {
   Drupal.behaviors.pdpBody = {
     attach(context) {
 
+      //snapScroll function
+      function _defineProperty(obj, key, value) {
+        if (key in obj) {
+          Object.defineProperty(obj, key, {
+            value: value,
+            enumerable: true,
+            configurable: true,
+            writable: true
+          });
+        } else {
+          obj[key] = value;
+        }
+    
+        return obj;
+      }
+    
+      function _objectSpread(target) {
+        for (var i = 1; i < arguments.length; i++) {
+          var source = arguments[i] != null ? arguments[i] : {};
+          var ownKeys = Object.keys(source);
+    
+          if (typeof Object.getOwnPropertySymbols === 'function') {
+            ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) {
+              return Object.getOwnPropertyDescriptor(source, sym).enumerable;
+            }));
+          }
+    
+          ownKeys.forEach(function (key) {
+            _defineProperty(target, key, source[key]);
+          });
+        }
+    
+        return target;
+      }
+    
+      function _toConsumableArray(arr) {
+        return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread();
+      }
+    
+      function _arrayWithoutHoles(arr) {
+        if (Array.isArray(arr)) {
+          for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) arr2[i] = arr[i];
+    
+          return arr2;
+        }
+      }
+    
+      function _iterableToArray(iter) {
+        if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter);
+      }
+    
+      function _nonIterableSpread() {
+        throw new TypeError("Invalid attempt to spread non-iterable instance");
+      }
+    
+      var SnapScroll = function SnapScroll(selector, options) {
+        var defaults = _objectSpread({
+          proximity: 100,
+          duration: 200,
+          easing: function easing(time) {
+            return time;
+          },
+          onSnapWait: 50
+        }, options);
+    
+        var items = _toConsumableArray(document.querySelectorAll(selector));
+    
+        var positions = [];
+        var currentlySnapped;
+        var snapTimeout;
+        var isScrolling;
+    
+        var getPositions = function getPositions() {
+          
+          console.log(items)
+          positions = items.map(function (item) {
+            return {
+              offset: $(item).is(":visible") ? item.getBoundingClientRect().top + window.scrollY : -5000,
+              element: item,
+            };
+          });
+          console.log(positions)
+        };
+    
+        var animatedScrollTo = function animatedScrollTo() {
+          var scrollTargetY = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+          var callback = arguments.length > 1 ? arguments[1] : undefined;
+          var _window = window,
+              scrollY = _window.scrollY;
+          var currentTime = 0;
+          var time = Math.max(0.1, Math.min(Math.abs(scrollY - scrollTargetY) / defaults.duration, 0.8));
+    
+          var tick = function tick() {
+            currentTime += 1 / 60;
+            var p = currentTime / time;
+            var t = defaults.easing(p);
+    
+            if (p < 1) {
+              requestAnimationFrame(tick);
+              window.scrollTo(0, scrollY + (scrollTargetY - scrollY) * t);
+            } else {
+              window.scrollTo(0, scrollTargetY);
+              callback();
+            }
+          };
+    
+          tick();
+        };
+    
+        var snapToElement = function snapToElement() {
+          var _window2 = window,
+              scrollY = _window2.scrollY;
+          var snapElement = positions.find(function (element) {
+            return element.offset - defaults.proximity <= scrollY && element.offset + defaults.proximity >= scrollY;
+          });
+          clearTimeout(snapTimeout);
+          console.log(currentlySnapped)
+    
+          if (snapElement && !isScrolling && snapElement != currentlySnapped) {
+            snapTimeout = setTimeout(function () {
+              isScrolling = true;
+              animatedScrollTo(snapElement.offset, function () {
+                isScrolling = !isScrolling;
+              });
+              currentlySnapped = snapElement;
+              console.log(currentlySnapped)
+            }, defaults.onSnapWait);
+          }
+        };
+    
+        var recalculateLayout = function recalculateLayout() {
+          getPositions();
+          snapToElement();
+        };
+    
+        var bindEvents = function bindEvents() {
+          window.addEventListener('resize', recalculateLayout);
+          window.addEventListener('scroll', snapToElement);
+        };
+    
+        var destroy = function destroy() {
+          window.removeEventListener('resize', recalculateLayout);
+          window.removeEventListener('scroll', snapToElement);
+        };
+    
+        var init = function init() {
+          getPositions();
+          bindEvents();
+        };
+    
+        init();
+        return {
+          init: init,
+          destroy: destroy,
+          recalculateLayout: recalculateLayout
+        };
+      };
+
       // scroll snapping
       var optionsMandatory = {
         proximity: 300,
       };
+      var snapScroller;
       if(context.querySelector('.pdp-body') !== null && window.innerWidth < 1024 ) {
-        SnapScroll('.scroll-mandatory', optionsMandatory);
+        snapScroller = SnapScroll('.scroll-mandatory', optionsMandatory);
+        setTimeout(() => {
+          snapScroller.recalculateLayout();
+        }, 300);
+        $(document).on('click', 'a[href^="#"]', function (e) {
+          snapScroller.recalculateLayout();
+          e.preventDefault();
+          $('html, body').animate({
+            //magic number for sticky header 
+            scrollTop: $($.attr(this, 'href')).offset().top
+          }, 600);
+        });
       }
       
 
@@ -82,7 +251,10 @@ import SnapScroll from 'snapscroll';
 
       var sizeElements = context.querySelectorAll('[data-size-id]');
       sizeElements.forEach((item) => {
-        item.addEventListener('click', e => updateSizeSlider(e, item.dataset.sizeId), false);
+        item.addEventListener('click', e => {
+          updateSizeSlider(e, item.dataset.sizeId);
+          snapScroller.recalculateLayout();
+        }, false);
       });
 
       //scroll effects: bubbles, section-select and WTB
