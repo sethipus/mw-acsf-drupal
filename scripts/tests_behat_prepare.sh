f516a21f0a15d4aa9b60c5960e2fa3612bbc4e80
Igor_Gaiduk <Igor_Gaiduk@epam.com>
2020-10-21 12:34:35 +0300

    reverted yml files

--- scripts/tests_behat_prepare.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -ev
-
-vendor/bin/blt setup --no-interaction --verbose
-composer require behat/mink-selenium2-driver
-
-set +v
--- scripts/tests_behat_run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -ev
-
-vendor/bin/blt tests:behat:run -D behat.web-driver=chrome --no-interaction --verbose
-
-set +v
