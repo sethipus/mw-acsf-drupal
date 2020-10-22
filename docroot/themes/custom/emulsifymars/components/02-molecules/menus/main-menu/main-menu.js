73db27dd9bfb3f9ab6c0fbe32acde03a326b5aac
Norbert Erdelyi <norbert.erdelyi@effem.com>
2020-10-22 14:04:09 +0200

    AB#192632: Added jQuery instead of vanilla js, removed unused main-menu-header.js.

--- docroot/themes/custom/emulsifymars/components/02-molecules/menus/main-menu/main-menu.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/menus/main-menu/main-menu.js
@@ -1,30 +1,24 @@
-Drupal.behaviors.mainMenu = {
-  attach(context) {
-    const toggleExpand = document.getElementById('toggle-expand');
-    const header = document.querySelector('#header-menu-trigger:not(.inited)');
-    if (header) {
-      const expandHeader = header.getElementsByClassName('main-menu__link--with-sub');
-
-      // Mobile Menu Show/Hide.
-      toggleExpand.addEventListener('click', e => {
-        toggleExpand.classList.toggle('toggle-expand--open');
-        header.classList.toggle('header__primary--open');
-        e.preventDefault();
-      });
-
-      // Expose mobile sub menu on click.
-      for (let i = 0; i < expandHeader.length; i += 1) {
-        expandHeader[i].addEventListener('click', e => {
-          if (window.innerWidth < 1024) {
+(function($) {
+  Drupal.behaviors.mainMenu = {
+    attach(context) {
+      (function(header) {
+        if (header.length) {
+          $('#toggle-expand').on('click', e => {
+            e.currentTarget.classList.toggle('toggle-expand--open');
+            $('#header-menu-trigger').toggleClass('header__primary--open');
             e.preventDefault();
-          }
-          const menuItem = e.currentTarget;
-          const subMenu = menuItem.nextElementSibling;
-          subMenu.classList.toggle('main-menu--sub-open');
-          subMenu.nextElementSibling.classList.toggle('main-menu--sub-open');
-        });
-      }
-      header.classList.add('inited');
-    }
-  },
-};
+          });
+          header.find('.main-menu__link--with-sub').on('click', e => {
+            if ($(window).width() < 1024) {
+              e.preventDefault();
+            }
+            const menuItem = e.currentTarget;
+            const subMenu = menuItem.nextElementSibling;
+            subMenu.classList.toggle('main-menu--sub-open');
+            subMenu.nextElementSibling.classList.toggle('main-menu--sub-open');
+          });
+        }
+      })($('#header-menu-trigger').once('menuInited'));
+    },
+  };
+})(jQuery);
