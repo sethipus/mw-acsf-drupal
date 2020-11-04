a90addf37f118cf0dfacdd5bed09c07eb1842d21
Peter Bartos <Peter_Bartos@epam.com>
2020-11-04 16:39:38 +0100

    AB#193865: Fixed content feature js to only run scroll function when element is in viewport - put throttle on scroll, fixed background image to zoom to the middle of the image on smaller screens

--- docroot/themes/custom/emulsifymars/components/02-molecules/content-feature/content-feature.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/content-feature/content-feature.js
@@ -1,27 +1,32 @@
-Drupal.behaviors.contentFeature = {
-  attach(context) {
-    if (context.getElementById('content-feature') === null) {
-      return;
-    }
-    window.addEventListener('scroll', () => {
-      const parallaxElement = context.getElementById('content-feature');
-      const offset = window.pageYOffset;
-      parallaxElement.style.backgroundPosition = `0 ${- (offset * .3)}px`;
-    })
-
-    const isInViewport = element => {
-      const scroll = window.scrollY || window.pageYOffset
-      const boundsTop = element.getBoundingClientRect().top + scroll
-      const viewport = {
-        top: scroll,
-        bottom: scroll + window.innerHeight,
+(function($, _){
+  Drupal.behaviors.contentFeature = {
+    attach(context) {
+      if (context.getElementById('content-feature') === null) {
+        return;
       }
-      const bounds = {
-        top: boundsTop,
-        bottom: boundsTop + element.clientHeight,
+  
+      $(window).on('scroll', _.throttle(() => {
+        const parallaxElement = context.getElementById('content-feature');
+        if (isInViewport(parallaxElement)){
+          const offset = window.pageYOffset;
+          parallaxElement.style.backgroundPosition = `50% ${- (offset * .3)}px`;
+        }
+      }, 50));
+      
+      const isInViewport = element => {
+        const scroll = window.scrollY || window.pageYOffset
+        const boundsTop = element.getBoundingClientRect().top + scroll
+        const viewport = {
+          top: scroll,
+          bottom: scroll + window.innerHeight,
+        }
+        const bounds = {
+          top: boundsTop,
+          bottom: boundsTop + element.clientHeight,
+        }
+        return (bounds.bottom >= viewport.top && bounds.bottom <= viewport.bottom) ||
+          (bounds.top <= viewport.bottom && bounds.top >= viewport.top);
       }
-      return (bounds.bottom >= viewport.top && bounds.bottom <= viewport.bottom) ||
-        (bounds.top <= viewport.bottom && bounds.top >= viewport.top);
     }
   }
-}
+})(jQuery, _);
