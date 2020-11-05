a7c4a49125f592da9e7ef1dfb9bcbeebd6faeea9
Peter Bartos <Peter_Bartos@epam.com>
2020-11-05 12:17:42 +0100

    AB#193865: Added loop when there are multiple content features on a page, removed id from content feature from template, as it caused multiple smae id-s on one page

--- docroot/themes/custom/emulsifymars/components/02-molecules/content-feature/content-feature.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/content-feature/content-feature.js
@@ -1,33 +1,31 @@
 (function($, Drupal, _){
   Drupal.behaviors.contentFeature = {
     attach(context) {
-      const parallaxElement = $('.content-feature', context);
+      $(context).find('.content-feature').once('contentFeature').each(function(){
+        const $contentFeature = $(this);
 
-      if (!parallaxElement.length) {
-        return;
-      }
-  
-      $(window).once('contentFeature').on('scroll', _.throttle(() => {
-        if (isInViewport(parallaxElement[0])){
-          const offset = window.pageYOffset;
-          parallaxElement.css('background-position', `50% ${- (offset * .3)}px`);
+        $(window).on('scroll', _.throttle(() => {
+          if (isInViewport($contentFeature[0])){
+            const offset = window.pageYOffset;
+            $contentFeature.css('background-position', `50% ${- (offset * .3)}px`);
+          }
+        }, 50));
+        
+        const isInViewport = element => {
+          const scroll = window.scrollY || window.pageYOffset
+          const boundsTop = element.getBoundingClientRect().top + scroll
+          const viewport = {
+            top: scroll,
+            bottom: scroll + window.innerHeight,
+          }
+          const bounds = {
+            top: boundsTop,
+            bottom: boundsTop + element.clientHeight,
+          }
+          return (bounds.bottom >= viewport.top && bounds.bottom <= viewport.bottom) ||
+            (bounds.top <= viewport.bottom && bounds.top >= viewport.top);
         }
-      }, 50));
-      
-      const isInViewport = element => {
-        const scroll = window.scrollY || window.pageYOffset
-        const boundsTop = element.getBoundingClientRect().top + scroll
-        const viewport = {
-          top: scroll,
-          bottom: scroll + window.innerHeight,
-        }
-        const bounds = {
-          top: boundsTop,
-          bottom: boundsTop + element.clientHeight,
-        }
-        return (bounds.bottom >= viewport.top && bounds.bottom <= viewport.bottom) ||
-          (bounds.top <= viewport.bottom && bounds.top >= viewport.top);
-      }
+      })
     }
   }
 })(jQuery, Drupal, _);
