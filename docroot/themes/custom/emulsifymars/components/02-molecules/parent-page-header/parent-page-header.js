964b94086caa736698166fc84984d26553bd97d4
Mate Nagy <mate_nagy1@epam.com>
2020-11-09 09:04:43 +0100

    AB#193199: revert accidental changes.

--- docroot/themes/custom/emulsifymars/components/02-molecules/parent-page-header/parent-page-header.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/parent-page-header/parent-page-header.js
@@ -1,26 +1,19 @@
-(function ($, Drupal) {
-Drupal.behaviors.parentPageHeader = {
+Drupal.behaviors.bgVideo = {
     attach(context) {
-      $(context).find('.parent-page-header').once('parentPageHeader').each(function(){
-        const $parentPageHeader = $(this);
-        const $button = $parentPageHeader.find('.video-button');
-        const $video = $parentPageHeader.find('.video-player');
-        if ($button.length < 1 || $video.length < 1) {
-          return;
+      const button = context.getElementById('video-button');
+      const video = context.getElementById('video-player');
+      if (video === null) {
+        return;
+      }
+      button.innerHTML = video.paused ? '||' : '>';
+      button.addEventListener('click', () => {
+        if(video.paused) {
+          video.play();
+          button.innerHTML = '||';
+        } else {
+          video.pause();
+          button.innerHTML = '>';
         }
-        const videoElement = $video[0];
-
-        $button.html(videoElement.paused ? '||' : '>');
-        $button.on('click', () => {
-          if(videoElement.paused) {
-            videoElement.play();
-            $button.html('||');
-          } else {
-            videoElement.pause();
-            $button.html('>');
-          }
-        });
       });
     },
   };
-})(jQuery, Drupal);
