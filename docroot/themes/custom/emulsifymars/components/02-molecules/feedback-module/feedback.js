c90f5cd5f28ea86996ffb185d27b0be4839ec12c
Kiryl Karaliou <kiryl_karaliou@epam.com>
2020-11-27 02:11:49 +0300

    AB#194304 Adds datalayer events for poll, feedback and contact us

--- docroot/themes/custom/emulsifymars/components/02-molecules/feedback-module/feedback.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/feedback-module/feedback.js
@@ -1,6 +1,38 @@
 (function($){
   Drupal.behaviors.feedback = {
     attach(context) {
+      const feedbackContainer = context.querySelector('.feedback-module');
+      const feedbackForm = feedbackContainer.closest('form');
+      const feedbackInputs = feedbackContainer.querySelectorAll('input');
+      const componentBlock = feedbackContainer.closest('[data-block-plugin-id]');
+      const componentName = componentBlock ? componentBlock.dataset.blockPluginId : '';
+
+      // Add event listeners to provide info to Data layer
+      if (typeof dataLayer !== 'undefined') {
+        feedbackInputs.forEach((input) => {
+          input.addEventListener('click', () => {
+            let clientId = '';
+            const cookies = document.cookie.split(';');
+            cookies.forEach(cookie => {
+              if(cookie.indexOf('_ga=') !== -1) {
+                clientId = cookie.trim().substring(4, cookie.length);
+              }
+            });
+
+            dataLayer.push({
+              pageName: document.title,
+              componentName: componentName,
+              formSubmitFlag: 1,
+              clientId: clientId,
+              formName: 'feedback',
+              formID: feedbackForm.id || '',
+              formfieldId: input.value || ''
+            });
+
+          });
+        });
+      }
+
       $('.feedback-module').once('feedback').each(function() {
         let radioBtn = $(this).find('.feedback-module__radio');
         let submitBtn = $(this).find('.button-vote');
