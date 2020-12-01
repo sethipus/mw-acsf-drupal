3c9aab97b14fed17e193e56de5a8c645b5484a4d
Mate Nagy <mate_nagy1@epam.com>
2020-12-01 21:25:33 +0100

    AB#000000: add all card variation to recommendations module and card grid. Use react server side rendering to reuse components. Fix minor issues.

--- docroot/themes/custom/emulsifymars/components/02-molecules/card/recipe-card/recipe-card.stories.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/card/recipe-card/recipe-card.stories.js
@@ -3,10 +3,15 @@ import React from 'react';
 import recipeCard from './recipe-card.twig';
 import recipeCardData from './recipe-card.yml';
 import recipeCardMaxLengthData from './recipe-card-max-length.yml';
+import recipeCardPairUpData from './recipe-card-content-pair-up.yml';
 
 export default {
   title: 'Molecules/Cards/Recipe Card',
   decorators:  [(Story) => <div style={{ padding: '5rem' }}><Story/></div>]
+};
+
+export const recipeCardContentPairUp = () => {
+  return <div dangerouslySetInnerHTML={{ __html: recipeCard({...recipeCardData, ...recipeCardPairUpData}) }} />
 };
 
 export const recipeCardExample = () => {
