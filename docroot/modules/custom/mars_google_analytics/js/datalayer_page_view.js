f7a6cf3349fcf79330fd872f527417d835df100f
Andrii Shutov <Andrii_Shutov@epam.com>
2020-12-03 16:07:07 +0200

    AB#189359: correct form processing tagging.

--- docroot/modules/custom/mars_google_analytics/js/datalayer_page_view.js
+++ docroot/modules/custom/mars_google_analytics/js/datalayer_page_view.js
@@ -31,7 +31,7 @@
       settings.dataLayer.products = gtins.join(', ');
 
       var dataElements = context.querySelectorAll('[data-datalayer-taxonomy]');
-      var taxonomy = (settings.dataLayer.taxonomy) ? JSON.parse(settings.dataLayer.taxonomy) : {};
+      var taxonomy = (settings.dataLayer.taxonomy !== null) ? JSON.parse(settings.dataLayer.taxonomy) : {};
       dataElements.forEach(function (product) {
         let taxonomy_info = JSON.parse(product.getAttribute('data-datalayer-taxonomy'));
 
@@ -49,8 +49,10 @@
       });
 
       var taxonomy_output = '';
-      for (const [key, value] of Object.entries(taxonomy)) {
-        taxonomy_output += key + ': ' + value.join(', ') + '; ';
+      if (taxonomy !== null) {
+        for (const [key, value] of Object.entries(taxonomy)) {
+          taxonomy_output += key + ': ' + value.join(', ') + '; ';
+        }
       }
 
       settings.dataLayer.taxonomy = taxonomy_output.trim();
@@ -232,86 +234,73 @@
         }
       });
 
-      // POLL MOUSEDOWN EVENT
-      const pollContainer = context.querySelector('.polling');
-      if (pollContainer) {
-        const pollInputs = pollContainer.querySelectorAll('input');
-        const pollSubmit = pollContainer.querySelector('.button-vote');
-        // Add event listeners to provide info to Data layer
-          setTimeout(function() {
-            pollSubmit.addEventListener('mousedown', () => {
-              // find what radio button was selected
-              const selectedInput = [...pollInputs].filter(input => input.checked)[0];
-              if (selectedInput) {
-                dataLayer.push({
-                  event: 'formfieldComplete',
-                  pageName: document.title,
-                  componentName: getComponentName(pollContainer),
-                  formSubmitFlag: 1,
-                  formName: 'Poll',
-                  formSelected: selectedInput.parentElement.innerText.trim()
-                });
-              }
 
+      // ENTRY GATE CLICK EVENT
+      //const entryGateContainer = context.querySelector('.entry-gate__inner');
+      //if (entryGateContainer) {
+      //  const entryGateSubmit = entryGateContainer.querySelector('.entry-gate-form__submit-btn');
+      //  // Add event listeners to provide info to Data layer
+      //  setTimeout(function() {
+      //    entryGateSubmit.addEventListener('click', () => {
+      //      const birthInputs = Array.from(entryGateContainer.querySelectorAll('input'));
+      //      const birthInputValues = birthInputs.map(el => el.value);
+      //      if (birthInputValues) {
+      //        dataLayer.push({
+      //          event: 'formfieldComplete',
+      //          pageName: document.title,
+      //          componentName: getComponentName(entryGateContainer),
+      //          formSubmitFlag: 1,
+      //          formName: 'Entry gate',
+      //          formSelected: birthInputValues
+      //        });
+      //      }
+      //    });
+      //  }, 100);
+      //}
+
+      var bindContactFormEvents = function(formContainer) {
+        var contactForm = formContainer.querySelector('form');
+        // find what fields of the form has value button was selected
+        const populatedFields = [contactForm.elements].filter(function(field) {
+          return field.value !== '';
+        });
+        Array.from(contactForm.elements).forEach((input) => {
+          if (input.type === 'button' || input.type === 'submit') {
+            input.addEventListener('mousedown', function(e) {
+              dataLayer.push({
+                event: 'formSubmit',
+                pageName: document.title,
+                componentName: getComponentName(formContainer),
+                formName: 'Contact & Help',
+                formSubmitFlag: 1,
+              });
             });
-          }, 100);
-      }
-
-      // CONTACT US CLICK EVENT
-      const formContainer = context.querySelector('.form-integration');
-      if (formContainer) {
-        var loading = false;
-        while (loading) {
-          var contactForm = formContainer.querySelector('form');
-          if (form !== null) {
-            var formSubmit = contactForm.querySelector('input[type=submit]');
-            loading = true;
           }
-        }
-        var submit = document.querySelector('input[type=submit]');
-        submit.addEventListener('click', function(e) {
-          // find what fields of the form has value button was selected
-          const populatedFields = [document.form.elements].filter(function(field) {
-            return field.value !== '';
-          });
-          let selectedValues = [];
-          for (let i=0; i < populatedFields.length; i++) {
-            const currentField = populatedFields[i];
-            selectedValues[currentField.name] = populatedFields.value;
+          else {
+            input.addEventListener('blur', function (e) {
+              dataLayer.push({
+                event: 'formFieldComplete',
+                pageName: document.title,
+                componentName: getComponentName(formContainer),
+                formName: 'Contact & Help',
+                formFieldName: e.target.name,
+                formFieldValue: e.target.value,
+              });
+            });
           }
-          dataLayer.push({
-            event: 'formFieldComplete',
-            pageName: document.title,
-            componentName: getComponentName(pollContainer),
-            formSubmitFlag: 1,
-            formName: 'Contact & Help',
-            formSelected: selectedValues
-          });
         });
       }
 
+      // POLL MOUSEDOWN EVENT
+      const pollContainer = context.querySelector('.poll-view');
+      if (pollContainer) {
+        bindContactFormEvents(pollContainer);
+      }
 
       // ENTRY GATE CLICK EVENT
       const entryGateContainer = context.querySelector('.entry-gate__inner');
       if (entryGateContainer) {
-        const entryGateSubmit = entryGateContainer.querySelector('.entry-gate-form__submit-btn');
-        // Add event listeners to provide info to Data layer
-        setTimeout(function() {
-          entryGateSubmit.addEventListener('click', () => {
-            const birthInputs = Array.from(entryGateContainer.querySelectorAll('input'));
-            const birthInputValues = birthInputs.map(el => el.value);
-            if (birthInputValues) {
-              dataLayer.push({
-                event: 'formfieldComplete',
-                pageName: document.title,
-                componentName: getComponentName(entryGateContainer),
-                formSubmitFlag: 1,
-                formName: 'Entry gate',
-                formSelected: birthInputValues
-              });
-            }
-          });
-        }, 100);
+        bindContactFormEvents(entryGateContainer);
       }
     }
   };
