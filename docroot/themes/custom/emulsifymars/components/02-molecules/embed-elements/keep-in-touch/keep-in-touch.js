a5734207f53e0a76b2c46294d87e47ca03617e25
Anton Tsyhanok <anton.tsyhanok@effem.com>
2020-12-04 00:25:24 +0200

    AB#189359: Embed forms events.

--- docroot/themes/custom/emulsifymars/components/02-molecules/embed-elements/keep-in-touch/keep-in-touch.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/embed-elements/keep-in-touch/keep-in-touch.js
@@ -3,7 +3,7 @@ Drupal.behaviors.keepInTouchForm = {
     /* Embed Code */
     $(document).ready(function() {
       /* <!-- Google Tag Manager --> */
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&amp;l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-M3VZDN2');
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&amp;l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-K5P8GLL');
       /* <!-- End Google Tag Manager --> */
       // Validating fields.
       function validateField(field) {
