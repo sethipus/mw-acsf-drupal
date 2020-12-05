5c5781fdb3e1893d7f9d5be863cf6261c5e02b75
Andrii Shutov <Andrii_Shutov@epam.com>
2020-12-05 14:45:40 +0200

    AB#182089: publish unit tests code coverage report.

--- scripts/phpunit_test.sh
+++ scripts/phpunit_test.sh
@@ -3,5 +3,7 @@
 set -ev
 
 vendor/bin/blt tests:phpunit:run --define drush.alias='${drush.aliases.ci}' -D behat.web-driver=chrome --no-interaction --ansi --verbose
+mkdir -p reports; mkdir -p reports/phpunit
+vendor/bin/phpunit -c phpunit.xml.dist --coverage-clover reports/phpunit/coverage.xml --verbose
 
 set +v
