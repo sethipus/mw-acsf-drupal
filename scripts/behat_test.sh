701f0c4bacbd0604a11f8c04ad1a5e1964744235
Igor_Gaiduk <Igor_Gaiduk@epam.com>
2020-12-08 12:03:11 +0300

    Running behat tests

--- scripts/behat_test.sh
+++ scripts/behat_test.sh
@@ -2,6 +2,6 @@
 
 set -ev
 
-vendor/bin/blt tests:behat:run --define drush.alias='${drush.aliases.ci}' -D behat.web-driver=chrome --no-interaction --ansi --verbose
+vendor/bin/blt tests:behat:run -D behat.web-driver=chrome --no-interaction --ansi --verbose
 
 set +v
