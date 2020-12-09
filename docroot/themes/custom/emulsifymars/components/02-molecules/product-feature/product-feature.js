21a4bae8e8e7018e91d8c9011d135ee17cd643e5
zubovsta <stanislau.zubovich@effem.com>
2020-12-09 17:20:27 +0300

    AB#211552, AB#211553, AB#211554, AB#211557: Product Feature
    AB#211552: [Product Feature] : [Height] - Component height is incorrect
    AB#211553: [Product Feature] : [Animation] - Background parallax effect is not implemented
    AB#211554: [Product Feature] : [Background] - Brand shape color/opacity is incorrect
    AB#211557: [Product Feature] : [Image] - Image position is incorrect

--- docroot/themes/custom/emulsifymars/components/02-molecules/product-feature/product-feature.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/product-feature/product-feature.js
@@ -3,9 +3,9 @@
     attach(context) {
       $(context).find('.product-feature').once('productFeature').each(function(){
         const productFeature = this;
-        var bubble_1_top = productFeature.querySelector('#product-feature__bubble--1').offsetTop;
-        var bubble_2_top = productFeature.querySelector('#product-feature__bubble--2').offsetTop;
-        var bubble_3_top = productFeature.querySelector('#product-feature__bubble--3').offsetTop;
+        var bubble_1_top = productFeature.querySelector('.product-feature__bubble--1').offsetTop;
+        var bubble_2_top = productFeature.querySelector('.product-feature__bubble--2').offsetTop;
+        var bubble_3_top = productFeature.querySelector('.product-feature__bubble--3').offsetTop;
 
         const isInViewport = element => {
           const rect = element.getBoundingClientRect();
@@ -37,16 +37,16 @@
               parallaxCoef = 0.5;
           }
           let offset = windowHeight - (rect.top + (rect.height * 0.7));
-          element.querySelector('#product-feature__bubble--1').style.top = `${(bubble_1_top) - (offset * parallaxCoef)}px`;
-          element.querySelector('#product-feature__bubble--2').style.top = `${(bubble_2_top) - (offset * parallaxCoef)}px`;
-          element.querySelector('#product-feature__bubble--3').style.top = `${(bubble_3_top) - (offset * parallaxCoef)}px`;
+          element.querySelector('.product-feature__bubble--1').style.top = `${(bubble_1_top) - (offset * parallaxCoef)}px`;
+          element.querySelector('.product-feature__bubble--2').style.top = `${(bubble_2_top) - (offset * parallaxCoef)}px`;
+          element.querySelector('.product-feature__bubble--3').style.top = `${(bubble_3_top) - (offset * parallaxCoef)}px`;
 
         };
 
         const restoreElementsPositions = (element) => {
-          element.querySelector('#product-feature__bubble--1').style.removeProperty("top");
-          element.querySelector('#product-feature__bubble--2').style.removeProperty("top");
-          element.querySelector('#product-feature__bubble--3').style.removeProperty("top");
+          element.querySelector('.product-feature__bubble--1').style.removeProperty("top");
+          element.querySelector('.product-feature__bubble--2').style.removeProperty("top");
+          element.querySelector('.product-feature__bubble--3').style.removeProperty("top");
 
         }
 
