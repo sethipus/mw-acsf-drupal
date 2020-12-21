60f1e984dd08b3a430dcb7c7cc43b4278a440067
lakatist <istvan.lakatos@effem.com>
2020-12-21 19:46:01 +0100

    AB#212357: Product content pair up unit tests.

--- docroot/modules/custom/mars_product/tests/src/Unit/Plugin/Block/ProductContentPairUpBlockTest.php
+++ docroot/modules/custom/mars_product/tests/src/Unit/Plugin/Block/ProductContentPairUpBlockTest.php
@@ -3,12 +3,15 @@
 namespace Drupal\Tests\mars_product\Unit\Plugin\Block;
 
 use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Config\ImmutableConfig;
 use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Core\Entity\EntityViewBuilderInterface;
+use Drupal\Core\Url;
 use Drupal\mars_common\LanguageHelper;
 use Drupal\mars_common\MediaHelper;
+use Drupal\mars_common\SVG\SVG;
 use Drupal\mars_product\Plugin\Block\ProductContentPairUpBlock;
+use Drupal\node\NodeInterface;
 use Drupal\Tests\UnitTestCase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Form\FormStateInterface;
@@ -25,12 +28,12 @@ class ProductContentPairUpBlockTest extends UnitTestCase {
     'id' => 'product_content_pair_up_block',
     'provider' => 'mars_product',
     'title' => 'Title',
-    'entity_priority' => 'article_first',
+    'entity_priority' => ProductContentPairUpBlock::ARTICLE_OR_RECIPE_FIRST,
     'lead_card_eyebrow' => 'Master Card Eyebrow',
     'lead_card_title' => 'Master Card Title',
     'cta_link_text' => 'CTA Link text',
     'supporting_card_eyebrow' => 'Supporting Card Eyebrow',
-    'background' => 'media:186',
+    'background' => 'media:1',
   ];
 
   private const DEFINITION = [
@@ -40,6 +43,8 @@ class ProductContentPairUpBlockTest extends UnitTestCase {
   ];
 
   private const PLUGIN_ID = 'product_content_pair_up_block';
+  private const BACKGROUND_SOURCE = 'test_source';
+  private const CTA_LINK_URL = 'https://test.test';
 
   /**
    * System under test.
@@ -98,20 +103,6 @@ class ProductContentPairUpBlockTest extends UnitTestCase {
   private $configMock;
 
   /**
-   * Mock.
-   *
-   * @var \Drupal\Core\Config\ImmutableConfig|\PHPUnit\Framework\MockObject\MockObject
-   */
-  private $immutableConfigMock;
-
-  /**
-   * Mock.
-   *
-   * @var \Drupal\Core\Entity\EntityStorageInterface|\PHPUnit\Framework\MockObject\MockObject
-   */
-  private $nodeStorage;
-
-  /**
    * {@inheritdoc}
    */
   protected function setUp() {
@@ -128,21 +119,6 @@ class ProductContentPairUpBlockTest extends UnitTestCase {
       $this->languageHelperMock,
       $this->mediaHelperMock
     );
-  }
-
-  /**
-   * Create all mocks for tests.
-   */
-  private function createMocks(): void {
-    $this->containerMock = $this->createMock(ContainerInterface::class);
-    $this->entityTypeManagerMock = $this->createMock(EntityTypeManager::class);
-    $this->nodeStorage = $this->createMock(EntityStorageInterface::class);
-    $this->immutableConfigMock = $this->createMock(ImmutableConfig::class);
-    $this->configMock = $this->createMock(ConfigFactoryInterface::class);
-    $this->formStateMock = $this->createMock(FormStateInterface::class);
-    $this->languageHelperMock = $this->createMock(LanguageHelper::class);
-    $this->themeConfiguratorParserMock = $this->createMock(ThemeConfiguratorParser::class);
-    $this->mediaHelperMock = $this->createMock(MediaHelper::class);
   }
 
   /**
@@ -195,7 +171,16 @@ class ProductContentPairUpBlockTest extends UnitTestCase {
   public function testBuildConfigurationFormProperly() {
     $config_form = $this->block->buildConfigurationForm([], $this->formStateMock);
     $this->assertCount(14, $config_form);
+    $this->assertArrayHasKey('title', $config_form);
     $this->assertArrayHasKey('entity_priority', $config_form);
+    $this->assertArrayHasKey('article_recipe', $config_form);
+    $this->assertArrayHasKey('product', $config_form);
+    $this->assertArrayHasKey('lead_card_eyebrow', $config_form);
+    $this->assertArrayHasKey('lead_card_title', $config_form);
+    $this->assertArrayHasKey('cta_link_text', $config_form);
+    $this->assertArrayHasKey('supporting_card_eyebrow', $config_form);
+    $this->assertArrayHasKey('background', $config_form);
+    $this->assertIsArray($config_form['background']);
   }
 
   /**
@@ -213,6 +198,143 @@ class ProductContentPairUpBlockTest extends UnitTestCase {
       $form_data,
       $this->formStateMock
     );
+  }
+
+  /**
+   * Test building block when main entity was provided.
+   */
+  public function testShouldBuildProperlyWithMainEntity() {
+    $configuration = $this->block->getConfiguration();
+    $configuration['article_recipe'] = '123';
+    // Background was not added in the entity browser form.
+    unset($configuration['background']);
+    $this->block->setConfiguration($configuration);
+
+    $build = $this->block->build();
+
+    $this->assertArrayHasKey('#lead_card_entity', $build);
+    $this->assertArrayHasKey('#lead_card_eyebrow', $build);
+    $this->assertArrayHasKey('#lead_card_title', $build);
+    $this->assertEquals(self::CTA_LINK_URL, $build['#cta_link_url']);
+    $this->assertArrayHasKey('#cta_link_text', $build);
+    $this->assertEquals(self::BACKGROUND_SOURCE, $build['#background']);
+    $this->assertEquals('product_content_pair_up_block', $build['#theme']);
+    // Supporting card was not added on purpose.
+    $this->assertArrayNotHasKey('#supporting_card_entity', $build);
+    $this->assertArrayNotHasKey('#supporting_card_entity_view', $build);
+  }
+
+  /**
+   * Test building block when supporting entity was provided.
+   */
+  public function testShouldBuildProperlyWithSupportingEntity() {
+    $configuration = $this->block->getConfiguration();
+    $configuration['entity_priority'] = ProductContentPairUpBlock::PRODUCT_FIRST;
+    $configuration['article_recipe'] = '123';
+    $this->block->setConfiguration($configuration);
+
+    $build = $this->block->build();
+
+    $this->assertArrayHasKey('#supporting_card_entity', $build);
+    $this->assertArrayHasKey('#supporting_card_entity_view', $build);
+    $this->assertEquals(self::BACKGROUND_SOURCE, $build['#background']);
+    $this->assertEquals('product_content_pair_up_block', $build['#theme']);
+    // Main card was not added on purpose.
+    $this->assertArrayNotHasKey('#lead_card_entity', $build);
+    $this->assertArrayNotHasKey('#lead_card_eyebrow', $build);
+    $this->assertArrayNotHasKey('#lead_card_title', $build);
+    $this->assertArrayNotHasKey('#cta_link_text', $build);
+  }
+
+  /**
+   * Create all mocks for tests.
+   */
+  private function createMocks(): void {
+    $this->containerMock = $this->createMock(ContainerInterface::class);
+    $nodeMock = $this->createNodeMock();
+    $nodeStorageMock = $this->createMock(EntityStorageInterface::class);
+    $nodeStorageMock
+      ->method('load')
+      ->willReturn($nodeMock);
+
+    $viewbuilderMock = $this->createMock(EntityViewBuilderInterface::class);
+    $viewbuilderMock
+      ->method('view')
+      ->willReturn([]);
+
+    $this->entityTypeManagerMock = $this->createMock(EntityTypeManager::class);
+    $this->entityTypeManagerMock
+      ->method('getStorage')
+      ->willReturn($nodeStorageMock);
+    $this->entityTypeManagerMock
+      ->method('getViewBuilder')
+      ->willReturn($viewbuilderMock);
+
+    $this->configMock = $this->createMock(ConfigFactoryInterface::class);
+    $this->formStateMock = $this->createMock(FormStateInterface::class);
+
+    $this->languageHelperMock = $this->createMock(LanguageHelper::class);
+    $this->languageHelperMock->method('translate')
+      ->will(
+        $this->returnCallback(
+          function ($arg) {
+            return $arg;
+          })
+      );
+
+    $this->themeConfiguratorParserMock = $this->createMock(ThemeConfiguratorParser::class);
+    $this->themeConfiguratorParserMock
+      ->method('getGraphicDivider')
+      ->willReturn(new SVG('<svg xmlns="http://www.w3.org/2000/svg" />', 'id'));
+    $this->themeConfiguratorParserMock
+      ->method('getBrandShapeWithoutFill')
+      ->willReturn(new SVG('<svg xmlns="http://www.w3.org/2000/svg" />', 'id'));
+
+    $this->mediaHelperMock = $this->createMock(MediaHelper::class);
+    $this->mediaHelperMock->method('getEntityMainMediaId')
+      ->willReturn(1);
+    $this->mediaHelperMock->method('getMediaParametersById')
+      ->willReturn(
+        [
+          'src' => self::BACKGROUND_SOURCE,
+          'title' => 'test_title',
+          'alt' => 'test_image_alt',
+        ]
+      );
+    $this->mediaHelperMock
+      ->method('getIdFromEntityBrowserSelectValue')
+      ->willReturn(1);
+  }
+
+  /**
+   * Mock recipe node.
+   *
+   * @return \PHPUnit\Framework\MockObject\MockObject
+   *   Mock node object.
+   */
+  private function createNodeMock() {
+    $urlMock = $this->getMockBuilder(Url::class)
+      ->disableOriginalConstructor()
+      ->getMock();
+
+    $urlMock->expects($this->any())
+      ->method('toString')
+      ->willReturn($urlMock)
+      ->willReturn(self::CTA_LINK_URL);
+
+    $node = $this->getMockBuilder(NodeInterface::class)
+      ->disableOriginalConstructor()
+      ->getMock();
+
+    $node->expects($this->any())
+      ->method('bundle')
+      ->willReturn('product');
+
+    $node->expects($this->any())
+      ->method('toUrl')
+      ->willReturn($urlMock);
+
+    return $node;
   }
 
 }
