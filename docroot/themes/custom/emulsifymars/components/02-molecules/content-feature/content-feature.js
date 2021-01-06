6b345ffcdcbfa11a0379a3129e077841fcc2ad2b
Mate Nagy <mate_nagy1@epam.com>
2021-01-06 14:56:34 +0100

    AB#215694: fixing the paralax effect and the relate bg image sizing.

--- docroot/themes/custom/emulsifymars/components/02-molecules/content-feature/content-feature.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/content-feature/content-feature.js
@@ -1,48 +1,85 @@
 (function($, Drupal, _){
   Drupal.behaviors.contentFeature = {
-    attach(context) {
-      $(context).find('.content-feature').once('contentFeature').each(function() {
+    attach: function (context) {
+      $(context).find('.content-feature').once('contentFeature').each(function () {
         const contentFeatureModule = this;
         const bgUrl = contentFeatureModule.getAttribute('data-bgurl');
-        const parallaxCoef = 0.2;
+        const parallaxCoef = 1;
 
         const isInViewport = element => {
-          const rect = element.getBoundingClientRect();
+          const boundingRect = element.getBoundingClientRect();
 
-          const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
-          const windowWidth = (window.innerWidth || document.documentElement.clientWidth);
+          const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+          const windowWidth = window.innerWidth || document.documentElement.clientWidth;
 
-          const vertInView = ((rect.top + (rect.height * 0.7)) <= windowHeight) && ((rect.top + rect.height) >= 0);
-          const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0);
+          const vertInView = boundingRect.top <= windowHeight && (boundingRect.top + boundingRect.height) >= 0;
+          const horInView = boundingRect.left <= windowWidth && (boundingRect.left + boundingRect.width) >= 0;
 
-          return (vertInView && horInView);
+          return vertInView && horInView;
         }
 
         const updateElementsPositions = (element) => {
-          const rect = element.getBoundingClientRect();
           const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
-          let offset = windowHeight - (rect.top + (rect.height * 0.7));
-          element.style.backgroundPosition = `center calc(50% - ${offset * parallaxCoef}px`;
+          const windowMiddle = windowHeight / 2;
+
+          const boundingRect = element.getBoundingClientRect();
+          const containerHeight = boundingRect.height;
+          const containerMiddle = boundingRect.top + containerHeight / 2;
+
+          const currentOffset = windowMiddle - containerMiddle;
+          const maxOffset = (windowHeight + containerHeight) / 2;
+          const parallaxEffectPercentage = currentOffset / maxOffset;
+
+          const parallaxOverflow = containerHeight * parallaxCoef;
+          const bgPositionOffset = parallaxOverflow / 2 * parallaxEffectPercentage;
+
+          element.style.backgroundPosition = `50% calc(50% - ${bgPositionOffset}px`;
         };
 
         const updateBGSize = element => {
-          const containerWidth = element.clientWidth;
-          const containerHeight = element.clientHeight;
           const image = document.createElement('img');
           image.onload = () => {
-            console.log({
-              'containerWidth': containerWidth,
-              'containerHeight': containerHeight,
-              'imageWidth': image.naturalWidth,
-              'imageHeight': image.naturalHeight
-            })
+            const imageWidth = image.naturalWidth;
+            const imageHeight = image.naturalHeight;
+            const imageAspect = imageWidth / imageHeight;
+
+            const containerHeight = element.clientHeight;
+            const containerWidth = element.clientWidth;
+
+            // The target dimensions what we should cover with the bg image.
+            const parallaxCorrection = containerHeight * parallaxCoef;
+            const targetHeight = containerHeight + parallaxCorrection;
+            const targetWidth = containerWidth;
+            const targetAspect = targetWidth / targetHeight;
+
+            let resizedHeight;
+            let resizePercentage;
+
+            if (targetAspect < imageAspect) {
+              //Resize based on height.
+              resizePercentage = targetHeight / imageHeight;
+            }
+            else {
+              //Resize based on width.
+              resizePercentage = targetWidth / imageWidth;
+            }
+
+            resizedHeight = imageHeight * resizePercentage;
+
+            element.style.backgroundSize = `auto ${resizedHeight}px`;
           };
           image.src = bgUrl;
         }
 
-        const scrollResizeListener = () => {
+        const scrollListener = () => {
           if (isInViewport(contentFeatureModule)) {
             updateElementsPositions(contentFeatureModule);
+          }
+        };
+
+        const resizeListener = () => {
+          if (isInViewport(contentFeatureModule)) {
+            updateBGSize(contentFeatureModule);
           }
         };
 
@@ -51,8 +88,8 @@
         }
 
         window.addEventListener('DOMContentLoaded', documentReadyListener);
-        window.addEventListener('scroll', scrollResizeListener);
-        window.addEventListener('resize', scrollResizeListener);
+        window.addEventListener('scroll', _.throttle(scrollListener, 33));
+        window.addEventListener('resize', _.throttle(resizeListener, 33));
       })
     }
   }
