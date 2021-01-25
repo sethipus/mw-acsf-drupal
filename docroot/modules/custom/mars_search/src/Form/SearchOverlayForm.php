80e05b90ee4e76973d19dd14e6138ec4436d6ec0
lakatist <istvan.lakatos@effem.com>
2021-01-25 10:42:04 +0100

    AB#221733: Search overlay placeholder is configurable.

--- docroot/modules/custom/mars_search/src/Form/SearchOverlayForm.php
+++ docroot/modules/custom/mars_search/src/Form/SearchOverlayForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\mars_search\Form;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Url;
@@ -41,19 +42,30 @@ class SearchOverlayForm extends FormBase {
   private $languageHelper;
 
   /**
+   * Config factory service.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  private $config;
+
+  /**
    * Constructs a new SearchOverlayForm.
    *
    * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
    *   The request stack object.
    * @param \Drupal\mars_common\LanguageHelper $language_helper
    *   The language helper service.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config
+   *   The config factory service.
    */
   public function __construct(
     RequestStack $request_stack,
-    LanguageHelper $language_helper
+    LanguageHelper $language_helper,
+    ConfigFactoryInterface $config
   ) {
     $this->requestStack = $request_stack;
     $this->languageHelper = $language_helper;
+    $this->config = $config;
   }
 
   /**
@@ -62,7 +74,8 @@ class SearchOverlayForm extends FormBase {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('request_stack'),
-      $container->get('mars_common.language_helper')
+      $container->get('mars_common.language_helper'),
+      $container->get('config.factory')
     );
   }
 
@@ -80,7 +93,7 @@ class SearchOverlayForm extends FormBase {
           'search-input__field',
         ],
         'autocomplete' => 'off',
-        'placeholder' => $this->languageHelper->translate('Search'),
+        'placeholder' => $this->config->get('mars_common.site_labels')->get('header_search_overlay'),
         'aria-label' => $this->languageHelper->translate('Search input field'),
         // This is needed for correct work of SearchQueryParser.
         'data-grid-id' => SearchQueryParserInterface::MARS_SEARCH_DEFAULT_SEARCH_ID,
