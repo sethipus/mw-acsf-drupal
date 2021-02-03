59a49066203e6dc11544b2eafdc9a2400a497a5a
Anton Zakharenka <Anton_Zakharenka@epam.com>
2021-02-03 20:10:06 +0300

    AB#214362: Deleted the class on pressing the button.

--- docroot/themes/custom/emulsifymars/components/02-molecules/card/recipe-card/recipe-card.stories.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/card/recipe-card/recipe-card.stories.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 import recipeCard from './recipe-card.twig';
 import recipeCardData from './recipe-card.yml';
+import { useEffect } from '@storybook/client-api';
 import recipeCardMaxLengthData from './recipe-card-max-length.yml';
 import recipeCardPairUpData from './recipe-card-content-pair-up.yml';
+
+import './recipe-cart';
 
 export default {
   title: 'Molecules/Cards/Recipe Card',
@@ -11,13 +14,16 @@ export default {
 };
 
 export const recipeCardContentPairUp = () => {
+  useEffect(() => Drupal.attachBehaviors(), []);
   return <div dangerouslySetInnerHTML={{ __html: recipeCard({...recipeCardData, ...recipeCardPairUpData}) }} />
 };
 
 export const recipeCardExample = () => {
+  useEffect(() => Drupal.attachBehaviors(), []);
   return <div dangerouslySetInnerHTML={{ __html: recipeCard(recipeCardData) }} />
 };
 
 export const recipeCardMaxLength = () => {
+  useEffect(() => Drupal.attachBehaviors(), []);
   return <div dangerouslySetInnerHTML={{ __html: recipeCard({...recipeCardData, ...recipeCardMaxLengthData}) }} />
 };
