f3dc6d703d47f582f04767b6c1fc50e6e0ae1101
Anton Zakharenka <Anton_Zakharenka@epam.com>
2021-02-12 10:53:42 +0300

    AB#214362: Added new animate.

--- docroot/themes/custom/emulsifymars/components/02-molecules/card/recipe-card/recipe-cart.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/card/recipe-card/recipe-cart.js
@@ -5,13 +5,22 @@
             const $recipeCard = $(this);
             const $cardCta = $recipeCard.find('.default-link');
   
+            
+            $recipeCard.on('mouseover', () => {
+              $cardCta.addClass('default-link--light')
+            });
+            $recipeCard.on('mouseleave', () => {
+              $cardCta.removeClass('default-link--light')
+            });
             $recipeCard.on('click', (e) => {
-              $cardCta.removeClass('default-link--small')
-              console.log('Click')
-               {
+              $cardCta.removeClass('default-link--light')
+              if (
+                !e.target.parentNode.classList.contains('where-to-buy')
+              ) {
                 window.location.href = $cardCta.attr('href');
               }
             });
+            WTBInit();
           })
       }
     }
