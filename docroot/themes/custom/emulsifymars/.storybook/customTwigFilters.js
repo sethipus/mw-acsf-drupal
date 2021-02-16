9900b3ccbbdda8a0f3f90b7a1b1c26960b600c71
Mate Nagy <mate_nagy1@epam.com>
2021-02-16 16:49:16 +0100

    AB#224108: fix square CF test image uri.

--- docroot/themes/custom/emulsifymars/.storybook/customTwigFilters.js
+++ docroot/themes/custom/emulsifymars/.storybook/customTwigFilters.js
@@ -5,7 +5,7 @@
  */
 module.exports = function (twigInstance) {
   const resizeTargets = {
-    'square': '/adaptivemedia/rendition/id_b7426c1bfd973d87d4423ae19beae46364725b73/name_17_TICKLE+KITTEN.png/17_TICKLE+KITTEN.png',
+    'square': '/adaptivemedia/rendition/id_aa268285cf566e4b09bcc9ff271ce21056c95fcc/name_5025838006241_T60.png/5025838006241_T60.png',
     'portrait': '/adaptivemedia/rendition/id_4b8b5d8f4493bfb109ea42761e9ac789188bea4c/name_10132858-iams-dry-cat-04.eps/10132858-iams-dry-cat-04.eps',
     'landscape': '/adaptivemedia/rendition/id_414d8458f02fb9624cfd18589f048b99ab221e5a/name_Crawling+Cat.png/Crawling+Cat.png.jpeg',
   };
