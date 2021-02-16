cece7151ce25d5b7a455b0383c0d6facf051cd0d
Anton Zakharenka <Anton_Zakharenka@epam.com>
2021-02-16 18:32:58 +0300

    AB#214362: It needs to be tested on a mobile device.

--- docroot/themes/custom/emulsifymars/components/02-molecules/card/recipe-card/recipe-cart.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/card/recipe-card/recipe-cart.js
@@ -5,22 +5,9 @@
             const $recipeCard = $(this);
             const $cardCta = $recipeCard.find('.default-link');
   
-            
-            $recipeCard.on('mouseover', () => {
-              $cardCta.addClass('default-link--light')
-            });
-            $recipeCard.on('mouseleave', () => {
-              $cardCta.removeClass('default-link--light')
-            });
             $recipeCard.on('click', (e) => {
-              $cardCta.removeClass('default-link--light')
-              if (
-                !e.target.parentNode.classList.contains('where-to-buy')
-              ) {
-                window.location.href = $cardCta.attr('href');
-              }
+              $cardCta.blur();
             });
-            WTBInit();
           })
       }
     }
