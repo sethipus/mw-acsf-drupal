6958adcfedb9d201b3e55198560f6c973c6d3ea9
samoiyar <yaroslav.samoilenko@effem.com>
2021-02-18 10:51:47 +0200

    AB#229658: Changed equal operator to '==' to skip strict type comparsion in getComponentConfig method.

--- docroot/modules/custom/mars_search/src/Controller/MarsSearchController.php
+++ docroot/modules/custom/mars_search/src/Controller/MarsSearchController.php
@@ -303,7 +303,7 @@ class MarsSearchController extends ControllerBase implements ContainerInjectionI
       }
       // Adding an additional probe to get config if grid is not specified
       // because the text color may be overridden.
-      if ($grid_id === 1 && !empty($config['override_text_color'])) {
+      if ($grid_id == 1 && !empty($config['override_text_color'])) {
         return $config;
       }
     }
