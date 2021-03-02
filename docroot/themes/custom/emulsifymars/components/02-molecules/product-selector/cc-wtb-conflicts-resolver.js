e0af2304a9beca6cdc5d03e158496de7b0ad828b
samoiyar <yaroslav.samoilenko@effem.com>
2021-03-02 18:01:09 +0200

    AB#223291: Removed unnecessary JS Code.

--- docroot/themes/custom/emulsifymars/components/02-molecules/product-selector/cc-wtb-conflicts-resolver.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/product-selector/cc-wtb-conflicts-resolver.js
@@ -6,16 +6,6 @@
         let $root_containers = $(context).find('.cci-root-container');
         $root_containers.attr('data-no-focus-lock', true);
         $('[id="widgetMain"]').parent().slice(1).remove();
-        setTimeout(function () {
-          $root_containers.find('div:empty:not([class*="Popup"]').each(function () {
-            let $parent = $(this).parent();
-            let $parent_id = $parent.attr('id');
-            if ($parent_id !== 'widgetMain') {
-              $parent.remove();
-            }
-          });
-          $('[id="widgetMain"]').parent().slice(1).remove();
-        }, 1000);
         $('.product-selector').attr('data-no-focus-lock', true);
       }
 
