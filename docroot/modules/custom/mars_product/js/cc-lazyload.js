e4d972907f5cdaca4d3c2a8dfd7b5c1d2f1f0fc3
Maliah Rajan M <maliah.rajan.m@effem.com>
2021-05-28 12:04:15 +0530

    AB#244486: Modified setTimeOut

--- docroot/modules/custom/mars_product/js/cc-lazyload.js
+++ docroot/modules/custom/mars_product/js/cc-lazyload.js
@@ -1,9 +1,13 @@
 $('.cc-fi-button').click(function() {
     if ($('.cci-root-container').length == 0) {
         $.getScript($('#cci-widget').attr('data-src')).done((script, textStatus) => {
-            setTimeout(() => {
-                $(this).trigger('click');
-            }, 3000);
+            const checkScriptLoaded = setInterval(() => {
+                if ($('#widgetMain').length > 0) 
+                {
+                    clearInterval(checkScriptLoaded); 
+                    $(this).trigger('click');
+                }
+            }, 100);
         });
     }
 });
--- docroot/modules/custom/mars_product/js/price-spider-lazyload.js
+++ docroot/modules/custom/mars_product/js/price-spider-lazyload.js
@@ -8,11 +8,14 @@ if(window.location.pathname == '/')
         if ($('.ps-enabled').length == 0) {
             
             $.getScript($('#ps-widget').attr('data-src')).done((script, textStatus) => {
-                setTimeout(() => {
-                    $(this).find('span:first').remove();
-                    $(this).removeClass('default-link default-link-- ');
-                    $(this).trigger('click');
-                }, 3000);
+                const checkScriptLoaded = setInterval(() => {
+                    if ($('.ps-enabled').length > 0) { 
+                        clearInterval(checkScriptLoaded);
+                        $(this).find('span:first').remove();
+                        $(this).removeClass('default-link default-link-- ');
+                        $(this).trigger('click');
+                    }
+                }, 100);
             });
             return false;
         }
