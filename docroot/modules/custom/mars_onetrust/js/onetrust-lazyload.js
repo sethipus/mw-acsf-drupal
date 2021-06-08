dc53b4acd9279cafd2183c6b8ee46a227c060d8a
Maliah Rajan M <maliah.rajan.m@effem.com>
2021-06-08 21:03:52 +0530

    AB#244486 - Google page speed fixes

---
+++ docroot/modules/custom/mars_onetrust/js/onetrust-lazyload.js
@@ -0,0 +1,11 @@
+function lazyLoadCookieBanner() {
+
+    if($('#onetrust-sdk').length > 0)
+    {
+        $('#onetrust-sdk').attr('src',$('#onetrust-sdk').attr('data-src'));
+        $.getScript($('#onetrust-sdk').attr('data-src'));
+    }
+
+} 
+  
+
