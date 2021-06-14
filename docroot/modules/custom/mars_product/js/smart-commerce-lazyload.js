f6ef1d3046ae8d343230a7107dae943d03dd0df6
Maliah Rajan M <maliah.rajan.m@effem.com>
2021-06-14 14:54:41 +0530

    AB#244486 - Smart commerce brand assets fixes.

--- docroot/modules/custom/mars_product/js/smart-commerce-lazyload.js
+++ docroot/modules/custom/mars_product/js/smart-commerce-lazyload.js
@@ -4,6 +4,9 @@
 $('.inline-search__link').click(() => {
     if(typeof SmartCart === "undefined")
     {
-        $.getScript($('#smart-commerce-widget').attr('data-src'));
+        $.getScript($('#smart-commerce-widget').attr('data-src')).done(() => {
+            $.getScript($('#smart-commerce-brand-js').attr('data-src'));
+            $.getScript($('#smart-commerce-brand-css').attr('data-src'));            
+        });
     }
 });
