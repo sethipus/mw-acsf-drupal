f5d667240b33ea3f67d98cfed1ef61b06017b26a
Maliah Rajan M <maliah.rajan.m@effem.com>
2021-07-21 16:19:20 +0530

    #AB288133, #AB288288 -  PDP - WTB Fix & Age Gate Fix.

--- docroot/modules/custom/mars_product/js/cc-lazyload.js
+++ docroot/modules/custom/mars_product/js/cc-lazyload.js
@@ -6,30 +6,31 @@ function _lazyLoadWhereToBuy() {
         $.getScript($('#cci-widget').attr('data-src'));
     }
 }
-
-$('.inline-search__link').click(() => {
-    _lazyLoadWhereToBuy();
-});
-
 let commerceConnetor = false;
-$(window).scroll(function(){
-    if (
-            (
-                isInView($('.ajax-card-grid__items')) 
-                || 
-                isInView($('div[data-block-plugin-id="product_content_pair_up_block"]')) 
-                || 
-                isInView($('div[data-block-plugin-id="recommendations_module"]'))
-                ||
-                isInView($('div[data-block-plugin-id="recipe_detail_body"]'))  
-            ) 
-            && 
-            !commerceConnetor
-        )
-    {
+
+(function($){
+    $('.inline-search__link').click(() => {
         _lazyLoadWhereToBuy();
-    }
-})
+    });
+    $(window).scroll(function(){
+        if (
+                (
+                    isInView($('.ajax-card-grid__items')) 
+                    || 
+                    isInView($('div[data-block-plugin-id="product_content_pair_up_block"]')) 
+                    || 
+                    isInView($('div[data-block-plugin-id="recommendations_module"]'))
+                    ||
+                    isInView($('div[data-block-plugin-id="recipe_detail_body"]'))  
+                ) 
+                && 
+                !commerceConnetor
+            )
+        {
+            _lazyLoadWhereToBuy();
+        }
+    })
+})(jQuery);
 
 function isInView(elem){
    return $(elem).length > 0 ? $(elem).offset().top - $(window).scrollTop() < $(elem).height() : false;
