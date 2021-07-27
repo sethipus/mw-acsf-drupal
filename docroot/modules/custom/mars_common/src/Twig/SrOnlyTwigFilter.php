6a2688ddcdcef7b5796e0250fe1926d8c74691ee
Artem Sosiedko <artem_sosiedko@epam.com>
2021-07-27 12:10:26 +0300

    AB#288333:BE [Accessibility] New tab opening notification.

---
+++ docroot/modules/custom/mars_common/src/Twig/SrOnlyTwigFilter.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Drupal\mars_common\Twig;
+
+use Drupal\Component\Utility\UrlHelper;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\mars_common\LanguageHelper;
+use Twig\TwigFilter;
+
+/**
+ * Class SrOnlyTwigFilter.
+ *
+ * @package Drupal\mars_common\Twig
+ */
+class SrOnlyTwigFilter extends \Twig_Extension {
+
+  use StringTranslationTrait;
+
+  /**
+   * Language helper service.
+   *
+   * @var \Drupal\mars_common\LanguageHelper
+   */
+  private $languageHelper;
+
+  /**
+   * MediaHelperFilters constructor.
+   *
+   * @param \Drupal\mars_common\LanguageHelper $language_helper
+   *   The language helper service.
+   */
+  public function __construct(LanguageHelper $language_helper) {
+    $this->languageHelper = $language_helper;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFilters() {
+    return [
+      new TwigFilter('srOnlyTwigForTextarea', [$this, 'srOnlyTwigPreprocessForTextarea']),
+    ];
+  }
+
+  /**
+   * Filter add span for screen reader.
+   *
+   * @param string $string
+   *   A string containing the English text to translate.
+   *
+   * @return string|null
+   *   An object that, when cast to a string, returns the translated string.
+   */
+  public function srOnlyTwigPreprocessForTextarea($string): ?string {
+    $regexp = "<a(.*)href=\"(.*)\"(.*)>(.*)<\/a>";
+    preg_match_all("/$regexp/siU", $string, $links, PREG_SET_ORDER);
+    if (!empty($links)) {
+      foreach ($links as $link) {
+        // Full link tag.
+        $full_link = $link[0];
+        // Link url.
+        $url = $link[2];
+        // Link text.
+        $link_text = $link[4];
+        $link_has_target_blank = FALSE;
+        if (strpos($link[1], '_blank') !== FALSE ||
+          strpos($link[3], '_blank') !== FALSE
+        ) {
+          $link_has_target_blank = TRUE;
+        }
+        if (!empty($link_text) &&
+          (UrlHelper::isExternal($url) || $link_has_target_blank)
+        ) {
+          $sr_text_link = $link_text . ' <span class="sronly">' . $this->t('(opens in new window)') . '</span>';
+          $result = str_replace($link_text, $sr_text_link, $full_link);
+          $string = str_replace($full_link, $result, $string);
+        }
+      }
+    }
+    return $string;
+  }
+
+}
