c15a17c9eb645c85bb0748622db25711803f0bc3
japor <jpr.mejoy@codeandtheory.com>
2021-07-28 04:09:39 -0400

    AB#286632 Remove drop_cap field

--- docroot/modules/custom/mars_common/src/Plugin/Block/TextBlock.php
+++ docroot/modules/custom/mars_common/src/Plugin/Block/TextBlock.php
@@ -65,12 +65,6 @@ class TextBlock extends BlockBase implements ContainerFactoryPluginInterface {
       '#default_value' => $config['header'] ?? '',
     ];
 
-    $form['drop_cap'] = [
-      '#type' => 'hidden',
-      '#title' => $this->t('Drop cap'),
-      '#value' => 0,
-    ];
-
     $form['body'] = [
       '#type' => 'text_format',
       '#title' => $this->t('Body'),
@@ -99,7 +93,6 @@ class TextBlock extends BlockBase implements ContainerFactoryPluginInterface {
     $config = $this->getConfiguration();
 
     $build['#content'] = $this->languageHelper->translate($config['body']['value']);
-    $build['#drop_cap'] = $config['drop_cap'];
     $build['#header'] = $config['header'];
 
     $build['#theme'] = 'text_block';
