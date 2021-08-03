a71a3033499a960671c8fda37c5f5f0627f66c07
Anton Tsyhanok <anton.tsyhanok@effem.com>
2021-08-03 16:38:40 +0300

    AB#290644: Clean stories JS file.

--- docroot/themes/custom/emulsifymars/components/02-molecules/flexible-framer/flexible-framer.stories.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/flexible-framer/flexible-framer.stories.js
@@ -5,19 +5,15 @@ import flexibleFramer from './flexible-framer.twig';
 import flexibleFramerData from './flexible-framer.yml';
 import relatedRecipesData from './flexible-framer-recipes.yml';
 
-import './flexible-framer';
-
 /**
  * Storybook Definition.
  */
 export default { title: 'Molecules/Flexible Framer' };
 
 export const flexibleFramerComponent = () => {
-  useEffect(() => Drupal.attachBehaviors(), []);
   return <div dangerouslySetInnerHTML={{ __html: "<div style='height: 300px; background-color: grey'></div>" + flexibleFramer(flexibleFramerData) }} />;
 };
 
 export const relatedRecipesComponent = () => {
-  useEffect(() => Drupal.attachBehaviors(), []);
   return <div dangerouslySetInnerHTML={{ __html: flexibleFramer(relatedRecipesData) }} />;
 };
