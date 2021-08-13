0ecd2697c756a5b2754fdaf38b4332dafadd22a6
Anton Tsyhanok <anton.tsyhanok@effem.com>
2021-08-13 16:42:05 +0300

    AB#289005: Ability to fill the form for email recipe data.

---
+++ docroot/themes/custom/emulsifymars/components/02-molecules/recipe-email-form/recipe-email-form-layout/recipe-email-form-layout.js
@@ -0,0 +1,43 @@
+(function($, Drupal){
+  Drupal.behaviors.recipeEmailForm = {
+    attach(context) {
+      const openPopupButton = $('.js-email-recipe-btn');
+      const recipeEmailLayout = $('.recipe-email-layout');
+      const recipeEmailLightbox = $('.recipe-email-lightbox');
+      const recipeEmailCloseButton = $('.recipe-email-close-btn');
+      const inputFields = $('.recipe-email .form-item');
+      const errorMsg = $('.email-recipe-message-box');
+      const formSubmitButton = $('.js-form-submit');
+      const recipeEmailForm = $('#recipe-email-form');
+
+      openPopupButton.on('click', (e) => {
+        $('.recipe-email-layout').addClass('recipe-email-layout--opened');
+        e.preventDefault();
+        e.stopPropagation();
+      });
+
+      // Popup open/close events
+      recipeEmailCloseButton.on('click', () => {
+        $('.recipe-email-layout').removeClass('recipe-email-layout--opened');
+      });
+
+      recipeEmailLayout.on('click', function(e) {
+        $('.recipe-email-layout').removeClass('recipe-email-layout--opened');
+      });
+
+      recipeEmailLightbox.on('click', function(e) {
+        e.stopPropagation();
+      });
+
+      // Validation events
+      inputFields.on('focus click', () => {
+        errorMsg.html('');
+        recipeEmailForm.removeClass('recipe-email-form--invalid');
+      });
+
+      formSubmitButton.on('click', () => {
+        recipeEmailForm.addClass('recipe-email-form--invalid');
+      });
+    },
+  };
+})(jQuery, Drupal);
---
+++ docroot/themes/custom/emulsifymars/components/02-molecules/recipe-email-form/recipe-email-form.stories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useEffect } from "@storybook/client-api";
+
+import recipeEmailForm from './recipe-email-form-layout/recipe-email-form-layout.twig';
+import recipeEmailFormData from './recipe-email-form-layout/recipe-email-form-layout.yml';
+
+import './recipe-email-form-layout/recipe-email-form-layout.js';
+
+export default { title: 'Molecules/Recipe Email Form' };
+
+export const recipeEmailFormExample = () => {
+  useEffect(() => Drupal.attachBehaviors(), []);
+  return <div dangerouslySetInnerHTML={{__html: recipeEmailForm(recipeEmailFormData)}}
+              style={{backgroundColor: '#ccc'}}/>
+};
