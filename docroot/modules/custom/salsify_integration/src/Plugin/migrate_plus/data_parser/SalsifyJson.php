795eac5398158b879db99bafab52cefed9706245
Artem Sosiedko <artem_sosiedko@epam.com>
2021-09-15 17:00:34 +0300

    AB#302347: [Salsify] One channel for many brands.

--- docroot/modules/custom/salsify_integration/src/Plugin/migrate_plus/data_parser/SalsifyJson.php
+++ docroot/modules/custom/salsify_integration/src/Plugin/migrate_plus/data_parser/SalsifyJson.php
@@ -2,7 +2,10 @@
 
 namespace Drupal\salsify_integration\Plugin\migrate_plus\data_parser;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\migrate_plus\Plugin\migrate_plus\data_parser\Json;
+use Drupal\salsify_integration\Form\ConfigForm;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Obtain JSON data for migration.
@@ -15,12 +18,82 @@ use Drupal\migrate_plus\Plugin\migrate_plus\data_parser\Json;
 class SalsifyJson extends Json {
 
   /**
+   * The theme config object.
+   *
+   * @var \Drupal\Core\Config\ImmutableConfig
+   */
+  private $config;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $config_factory) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->config = $config_factory->get('salsify_integration.settings');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory')
+    );
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function currentUrl() {
     $keys = array_keys($this->urls);
     $index = $this->activeUrl ?: array_shift($keys);
     return isset($this->urls[$index]) ? $this->urls[$index] : NULL;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getSourceData($url) {
+    $response = $this->getDataFetcherPlugin()->getResponseContent($url);
+    // Convert objects to associative arrays.
+    $source_data = json_decode($response, TRUE);
+    $brand_name = $this->config->get('salsify_multichannel_approach.' . ConfigForm::BRAND_NAME);
+    $brand_name_trimmed = trim($brand_name);
+    // Check is brand name established in
+    // configuration.
+    if (!empty($brand_name_trimmed)) {
+      foreach ($source_data as $key => $product) {
+        $product_brand_name = isset($source_data[$key]['field_brand_name']) ? reset($source_data[$key]['field_brand_name']) : '';
+        // Unset products without brand field or
+        // not equals to brand from configuration.
+        if ($product_brand_name != $brand_name_trimmed) {
+          unset($source_data[$key]);
+        }
+      }
+    }
+
+    // If json_decode() has returned NULL, it might be that the data isn't
+    // valid utf8 - see http://php.net/manual/en/function.json-decode.php#86997.
+    if (is_null($source_data)) {
+      $utf8response = utf8_encode($response);
+      $source_data = json_decode($utf8response, TRUE);
+    }
+    // Backwards-compatibility for depth selection.
+    if (is_int($this->itemSelector)) {
+      return $this->selectByDepth($source_data);
+    }
+
+    // Otherwise, we're using xpath-like selectors.
+    $selectors = explode('/', trim($this->itemSelector, '/'));
+    foreach ($selectors as $selector) {
+      if (!empty($selector)) {
+        $source_data = $source_data[$selector];
+      }
+    }
+    return $source_data;
   }
 
 }
