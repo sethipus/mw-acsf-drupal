c5e1c8756fab1d1ab91a6d05c35f6d7fa86df893
DDEV-Local User <nobody@example.com>
2021-10-05 12:09:36 +0000

    AB#308230: Config split settings based on environments.

--- docroot/sites/default/settings.config_split.php
+++ docroot/sites/default/settings.config_split.php
@@ -11,8 +11,9 @@ use Acquia\Blt\Robo\Common\EnvironmentDetector;
  * Configuration split settings for environments.
  *
  * At anytime one configuration is recommended per environment.
- * If any new split is created, same should be updated in this file. This configurations is
- * used for setting the split for appropriate environment from environment variable.
+ * If any new split is created, same should be updated in this file.
+ * This configurations is used for setting the split for appropriate environment
+ * from environment variable.
  *
  * Remove this code if config_spit module is not used.
  */
@@ -24,16 +25,22 @@ $config['config_split.config_split.ci']['status'] = FALSE;
 $config['config_split.config_split.dev']['status'] = FALSE;
 $config['config_split.config_split.ah_other']['status'] = FALSE;
 $config['config_split.config_split.local']['status'] = FALSE;
+
 if ($env == '01live') {
   $config['config_split.config_split.prod']['status'] = TRUE;
-}elseif ($env == "01test") {
+}
+elseif ($env == "01test") {
   $config['config_split.config_split.stage']['status'] = TRUE;
-}elseif (EnvironmentDetector::isCiEnv()) {
+}
+elseif (EnvironmentDetector::isCiEnv()) {
   $config['config_split.config_split.ci']['status'] = TRUE;
-}elseif ($env == "01dev") {
+}
+elseif ($env == "01dev") {
   $config['config_split.config_split.dev']['status'] = TRUE;
-}elseif ($env == "0101qa") {
+}
+elseif ($env == "0101qa") {
   $config['config_split.config_split.ah_other']['status'] = TRUE;
-}elseif (EnvironmentDetector::isLocalEnv()) {
+}
+elseif (EnvironmentDetector::isLocalEnv()) {
   $config['config_split.config_split.local']['status'] = TRUE;
 }
