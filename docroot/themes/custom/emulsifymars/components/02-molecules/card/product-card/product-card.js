5dcbd3923f458c731b47205910994b677a3dc384
kusuma236 <kusumakokurla@gmail.com>
2022-05-06 14:34:27 +0530

    AB#330692: when user clicks on see details cta where to buy cta, the CTA color is also changing and looks like disappearing.

--- docroot/themes/custom/emulsifymars/components/02-molecules/card/product-card/product-card.js
+++ docroot/themes/custom/emulsifymars/components/02-molecules/card/product-card/product-card.js
@@ -1,37 +1,38 @@
 (function ($, Drupal, _) {
-    let WTBInit = _.debounce(
-      function () {
-        if (
-          typeof (window.PriceSpider) !== 'undefined' &&
-          typeof (window.PriceSpider.rebind) === 'function'
-        ) {
-          window.PriceSpider.rebind();
-        }
-      },
-      200
-    );
-    Drupal.behaviors.productCard = {
-      attach(context) {
-        $(context).find('.product-card').once('productCard').each(function(){
-          const $productCard = $(this);
-          const $cardCta = $productCard.find('.default-link');
-
-          $productCard.on('mouseover', () => {
-            if(window.screen.availWidth > 768) {
-              $cardCta.addClass('default-link--light')
-            }
-
-          });
-          $productCard.on('mouseleave', () => {
-            $cardCta.removeClass('default-link--light')
-          });
-          $productCard.on('click', (e) => {
-            if (!$(e.target).closest('.where-to-buy').length) {
-              window.location.href = $cardCta.attr('href');
-            }
-          });
-          WTBInit();
-        })
+  let WTBInit = _.debounce(
+    function () {
+      if (
+        typeof (window.PriceSpider) !== 'undefined' &&
+        typeof (window.PriceSpider.rebind) === 'function'
+      ) {
+        window.PriceSpider.rebind();
       }
+    },
+    200
+  );
+  Drupal.behaviors.productCard = {
+    attach(context) {
+      $(context).find('.product-card').once('productCard').each(function(){
+        const $productCard = $(this);
+        const $cardCta = $productCard.find('.default-link');
+        
+        $productCard.on('mouseover', () => {
+          if(window.screen.availWidth >768) {
+            $cardCta.addClass('default-link--light')
+          }
+
+        });
+        $productCard.on('mouseleave', () => {
+          $cardCta.removeClass('default-link--light')
+        });
+        $productCard.on('click', (e) => {
+          $cardCta.removeClass('default-link--light');
+          if (!$(e.target).closest('.where-to-buy').length) {
+            window.location.href = $cardCta.attr('href');
+          }
+        });
+        WTBInit();
+      })
     }
-  })(jQuery, Drupal, _);
+  }
+})(jQuery, Drupal, _);
